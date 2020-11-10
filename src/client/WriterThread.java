db7734829c1e20ba28c409057905f963b66bff97
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-11-10 14:26:03 +0100

    add variables and methods to WriterThread

--- src/client/WriterThread.java
+++ src/client/WriterThread.java
@@ -1,7 +1,19 @@
 package client;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class WriterThread {
-    public WriterThread() {
+
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatClient client;
+
+    public WriterThread(Socket socket, ChatClient client) {
+            this.socket = socket;
+            this.client = client;
+    }
+    public void run(){
 
     }
     public static void main (String [] args){
