c03fed7ce3e01c24e3f1659a17ced47957ce4bc3
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-11-11 20:11:56 +0100

    ReaderThread: added method body of run()

--- src/client/ReaderThread.java
+++ src/client/ReaderThread.java
@@ -1,12 +1,47 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class ReaderThread {
+/**
+ *
+ */
+public class ReaderThread extends Thread{
+    private ChatClient client;
 
-    public ReaderThread(Socket socket, ChatClient client) {
+    private Socket socket;
+    private BufferedReader reader;
+
+    public ReaderThread(Socket socket,ChatClient client) {
+        this.socket = socket;
+        this.client = client;
     }
 
-    public void start() {
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        while (true){
+            try {
+                InputStream inStream = socket.getInputStream();
+                Scanner inScanner = new Scanner(inStream);
+                while (inScanner.hasNext()){
+                    String input = inScanner.next();
+                    System.out.println(input);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                String text = reader.readLine();
+                System.out.println(text);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
