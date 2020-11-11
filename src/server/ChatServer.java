c876af18d91270eabf93c24bb9939b2754fbd5bd
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-11 13:14:32 +0100

    added attributes and run-method to UserThread

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -2,4 +2,29 @@ package server;
 
 public class ChatServer {
 
+    
+    public void communicateAll(String message){
+        //delivers a message from one user to the all users
+    }
+
+    public void communicateOthers(String message, UserThread exceptMe){
+        //delivers a message from one user to others
+    }
+
+    public void justUser(String message, UserThread thisUser){
+        //sends a message only to one client
+    }
+
+    public void addUserName(String userName){
+       //store username in newly connected client
+    }
+
+    public void removeUser(String userNAme, UserThread thisUser){
+        //removes associated username and UserThread when client is disconnected
+    }
+
+
+
+
+
 }
--- src/server/UserThread.java
+++ src/server/UserThread.java
@@ -1,5 +1,60 @@
 package server;
 
-public class UserThread {
-   
+import java.io.*;
+import java.net.Socket;
+
+public class UserThread extends Thread{
+    private Socket socket;
+    private ChatServer server;
+    private PrintWriter userOut;
+
+  private UserThread(Socket socket, ChatServer server){
+      this.socket = socket;
+      this.server = server;
+  }
+    /**
+     * This method starts a new thread when ever a client gets connected to the server, therefore the server is able to handle multiple clients at the same time.
+     * It sends a welcome-message when a new user enters the chatroom and notifies all other users.
+     * It runs a loop of reading messages sent from the user and sending them to all other users until the user types "bye" to disconnect.
+     * The other users get notified when a user disconnects and the connection is closed.
+     */
+    @Override
+    public void run() {
+      try {
+          InputStream input = socket.getInputStream();
+          BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+          OutputStream output = socket.getOutputStream();
+          userOut = new PrintWriter(output, true);
+
+          String userName = reader.readLine();
+          server.addUserName(userName);
+
+          String serverMessage = "Welcome " + userName;
+          server.justUser(serverMessage, this);
+
+          serverMessage = userName + " joined the room.";
+          server.communicateOthers(serverMessage, this);
+
+          String clientMessage;
+
+          do {
+              clientMessage = reader.readLine();
+              serverMessage = userName + ": " + clientMessage;
+              server.communicateAll(serverMessage);
+
+          } while (!clientMessage.equals("bye"));
+          server.removeUser(userName, this);
+          socket.close();
+
+          serverMessage = userName + " left the room.";
+          server.communicateOthers(serverMessage, this);
+
+      } catch (IOException ex) {
+          System.out.println("Error occurred in UserThread: " + ex.getMessage());
+          ex.printStackTrace();
+      }
+
+    }
+
 }
