7cf47a88c656469414961f0238704c347c366fc2
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-11-12 10:18:02 +0100

    Using BufferedReader instead of Scanner in ReaderThread

--- src/client/ReaderThread.java
+++ src/client/ReaderThread.java
@@ -3,38 +3,36 @@ package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ReaderThread extends Thread{
     private ChatClient client;
 
     private Socket socket;
-    private BufferedReader reader;
+    private BufferedReader bReader;
 
     public ReaderThread(Socket socket,ChatClient client) {
         this.socket = socket;
         this.client = client;
+
+        try {
+            InputStream in = socket.getInputStream();
+            bReader = new BufferedReader(new InputStreamReader(in));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         while (true){
             try {
-                InputStream inStream = socket.getInputStream();
-                Scanner inScanner = new Scanner(inStream);
-                while (inScanner.hasNext()){
-                    String input = inScanner.next();
-                    System.out.println(input);
-                }
+                String text = bReader.readLine();
+                System.out.println(text + "\r\n");
             } catch (IOException e) {
                 e.printStackTrace();
-            }
-            try {
-                String text = reader.readLine();
-                System.out.println(text);
-            } catch (IOException e) {
-                e.printStackTrace();
+                break;
             }
         }
     }
