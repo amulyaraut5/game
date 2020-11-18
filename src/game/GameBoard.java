d7133b73aa6056a465b2e85a525b8826d9b19825
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-11-18 10:23:12 +0100

    add constructor GameBoard and Player and methods addUser and getPlayerCount and arraylist which stores the users

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -1,4 +1,35 @@
 package game;
 
+import server.UserThread;
+
+import java.util.ArrayList;
+
 public class GameBoard {
+    public ArrayList<Player> playerList;
+    private boolean started = false;
+
+    public GameBoard(){
+
+    }
+
+    /**
+     * It creates a Player and adds it to the list of joined player
+     * @param user Thread of the user
+     * @param username
+     */
+    public void addUser(UserThread user, String username){
+        Player player = new Player(user, username);
+        playerList.add(player);
+    }
+
+    /**
+     * It returns how many player already joined
+     * @return number of player
+     */
+    public int getPlayerCount(){
+        return playerList.size();
+    }
+
+
+
 }
--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,10 @@
 package game;
 
+import server.UserThread;
+
 public class Player {
 
+    public Player(UserThread user, String username) {
+        //kannst du auch ändern, ich wollte nur Fehlermeldungen vermeiden :)
+    }
 }
