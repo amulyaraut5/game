b37968c41e6ff3b963aed517c7bbd3b8bacce685
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-11-18 13:18:41 +0100

    comments in GameController to quickly summarize my thoughts about first connections of GameController and GameBoard

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -1,4 +1,13 @@
 package game;
 
 public class GameController {
+
+    //play: new GameBoard and then method addUser(userThread, username)
+
+    //es gibt Methode isStarted() in GameBoard um zu prüfen, ob Spiel bereits gestartet ist und man nicht mehr joinen kann
+    //und es gibt Methode getPlayerCount() in GameBoard um zu schauen, ob schon mehr als 2 Player und max 4 da sind
+
+
+    //method to send something from GameBoard to GameController and then just to the playing users
+    // sendMessage(String message, ArrayList<Player> playerList)
 }
