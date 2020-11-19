6917cfb9be6346ec2cd1a1feff12c423f84ff1f6
unterberg <unterberg@cip.ifi.lmu.de>
2020-11-19 10:19:52 +0100

    added descriptions for rudimental structure in GameController

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -9,18 +9,28 @@ public class GameController {
     }
 
     public void readCommand (String message, UserThread sender) {
-
+        //verwaltet jegliche commands, die vom server gesendet werden
     }
 
     public boolean isStarted () {
+        //überprüft, ob bereits ein Spiel gestartet wurde
         return true;
+    }
+
+    public int getNumberOfPlayers () {
+        //returns Number of Players that already joined the game
     }
 
     public void playGame (UserThread sender, String userName) {
 
         GameBoard gameBoard = new GameBoard();
-
+        // isStarted() überprüft, ob Spiel bereits gestartet
+        // wenn ja, Rückmeldung an User
+        // wenn nein, überprüfen, wie viele Spieler bereits beigetreten sind
+        // wenn <4, dann addUser
     }
+
+
 
 
     //play: new GameBoard and then method addUser(userThread, username)
