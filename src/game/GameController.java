24e0298d1c9e1842955ca5c8453b1a9b725f8f55
unterberg <unterberg@cip.ifi.lmu.de>
2020-11-19 12:59:15 +0100

    Refined Structure in GameController

--- src/game/GameController.java
+++ src/game/GameController.java
@@ -2,7 +2,10 @@ package game;
 
 import server.UserThread;
 
+import java.util.ArrayList;
+
 public class GameController {
+    private int playerCount;
 
     public GameController () {
 
@@ -17,28 +20,17 @@ public class GameController {
         return true;
     }
 
-    public int getNumberOfPlayers () {
-        //returns Number of Players that already joined the game
-    }
-
     public void playGame (UserThread sender, String userName) {
 
         GameBoard gameBoard = new GameBoard();
         // isStarted() überprüft, ob Spiel bereits gestartet
         // wenn ja, Rückmeldung an User
         // wenn nein, überprüfen, wie viele Spieler bereits beigetreten sind
-        // wenn <4, dann addUser
+        // wenn <4, dann addUser(userThread, userName)
     }
 
+    public void sendMessage (String message, ArrayList<Player> playerList) {
+        //method to send something from GameBoard to GameController and then just to the playing users
+    }
 
-
-
-    //play: new GameBoard and then method addUser(userThread, username)
-
-    //es gibt Methode isStarted() in GameBoard um zu prüfen, ob Spiel bereits gestartet ist und man nicht mehr joinen kann
-    //und es gibt Methode getPlayerCount() in GameBoard um zu schauen, ob schon mehr als 2 Player und max 4 da sind
-
-
-    //method to send something from GameBoard to GameController and then just to the playing users
-    // sendMessage(String message, ArrayList<Player> playerList)
 }
