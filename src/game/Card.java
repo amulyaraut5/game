0797bd9039f4631db321d0b1eb8a6c02d87a271a
Amulya Raut <raut@cip.ifi.lmu.de>
2020-11-19 14:32:17 +0100

    made a card class

--- src/game/Card.java
+++ src/game/Card.java
@@ -1,4 +1,34 @@
 package game;
 
 public class Card {
+    public static final int PRINCESS = 8;
+    public static final int COUNTESS = 7;
+    public static final int KING     = 6;
+    public static final int PRINCE   = 5;
+    public static final int MAID     = 4;
+    public static final int BARON    = 3;
+    public static final int PRIEST   = 2;
+    public static final int GUARD    = 1;
+
+
+
+
+    // array at index 0 is not the card
+    public final static int[] numberOfIndividualCards = {
+            0, 5, 2, 2, 2, 2, 1, 1, 1
+    };
+    public static final String[] nameOfCards = {
+            "", "Guard", "Priest", "Baron", "Handmaid", "Prince", "King", "Countess", "Princess"};
+
+
+    public int value;   // card's value as printed on the card. (1-8)
+
+    public Card(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return Integer.toString(value);
+    }
+
 }
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,16 +1,15 @@
 package server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class ChatServer {
-    private final int port;
+    private int port;
     private ArrayList<UserThread> userThreads = new ArrayList<>();
     private ArrayList<String> userNames = new ArrayList<>();
 
-    public ChatServer(int port) {
+    public ChatServer(int port) { this.port = port;
         this.port = port;
     }
 
@@ -25,26 +24,26 @@ public class ChatServer {
     /**
      * start_Server() method opens a channel for the connection between Server and Client
      */
-    private void start_Server() {
+    private  void start_Server(){
+
+        ServerSocket server_socket = null;
         try {
-            ServerSocket server_socket = new ServerSocket(port);
-            System.out.println("Chat server is waiting for clients to connect to port " + port + ".");
+            server_socket = new ServerSocket(port);
+            System.out.println("Chat server is waiting for the connection to: " + port);
             acceptClients(server_socket);
+
         } catch (IOException e) {
             System.err.println("could not connect: " + e.getLocalizedMessage());
             e.printStackTrace();
         }
 
     }
-
     /**
      * This method accepts the clients request and ChatServer assigns a separate thread to handle multiple clients
-     *
      * @param server_socket socket from which connection is to be established
      */
-    public void acceptClients(ServerSocket server_socket) {
-        boolean accept = true;
-        while (accept) {
+    public void acceptClients(ServerSocket server_socket)  {
+        while(true){
             Socket client_socket = null;
             try {
                 client_socket = server_socket.accept();
@@ -53,20 +52,15 @@ public class ChatServer {
                 UserThread newUser = new UserThread(client_socket, this);
                 userThreads.add(newUser);
                 newUser.start();
-            } catch (IOException e) {
-                accept = false;
-                System.out.println("Accept failed on: " + port);
+            }catch (IOException e){
+                System.out.println("Accept failed on:" + port);
             }
         }
     }
-
-
-    /**
-     * This method sends a message to each client which is connected to the server except the sender itself
-     */
-    public void communicate(String message, UserThread sender) {
-        for (UserThread user : userThreads) {
-            if (user != sender) {
+    
+    public void communicate(String message, UserThread sender){
+        for (UserThread user: userThreads){
+            if (user != sender){
                 user.sendMessage(message);
             }
 
@@ -74,34 +68,40 @@ public class ChatServer {
     }
 
 
+    public void justUser(String message, UserThread thisUser){
+        for (UserThread aUser : userThreads) {
+            if (aUser == thisUser) {
+                aUser.sendMessage(message);
+            }
+        }//sends a message only to one client
+    }
     /**
-     * After the UserThread is created and user enters the name, the new user is added to the Set of the names.
+     * After the Userthread is created and user enters the name, the new user is added to the Set of the names.
      *
      * @param userName userName to be added
      */
-    public void addUserName(String userName) {
+    public void addUserName(String userName){
         userNames.add(userName);
     }
-
     /**
-     * It checks if the username is already in the list of assigned usernames.
-     *
+     * It checks if the username is already in the list of assigned usernames
+     * It also makes sure that the user enters something and not a empty String.
      * @param userName userName to be checked
      * @return True if username is free, false if it´s already assigned
      */
-    public boolean checkAvailability(String userName) {
+    public boolean checkAvailability (String userName){
         return (!userNames.contains(userName));
     }
-
     /**
-     * This method removes the username and UserThread from their respective Set.
+     * This method removes the username and userthread from their respective Set .
      * Removing can be done by calling a pre defined method remove().
-     *
      * @param userName userName to be removed
-     * @param thisUser UserThread to be removed
+     * @param thisUser userthread to be removed
      */
-    public void removeUser(String userName, UserThread thisUser) {
-        userNames.remove(userName);
-        userThreads.remove(thisUser);
+    public void removeUser(String userName, UserThread thisUser){
+        if (userNames.remove(userName)) {
+            userThreads.remove(thisUser);
+            System.out.println(userName + " quitted") ;
+        }
     }
 }
