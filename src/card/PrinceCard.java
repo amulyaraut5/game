18edda239bfb1074e55b6da51c7a6768cf79a239
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-21 20:01:29 +0100

    added method getTargetPlayer in prince

--- src/card/PrinceCard.java
+++ src/card/PrinceCard.java
@@ -2,9 +2,11 @@ package card;
 
 import game.Player;
 
-import java.util.ArrayList;
-
 public class PrinceCard extends Card {
+    Player targetPlayer = null;
+    String targetPlayerName = "";
+    Player playerPlayingCard = null;
+
     public PrinceCard(String nameOfCard, int cardValue) {
         this.nameOfCard = nameOfCard;
         this.cardValue = cardValue;
@@ -20,7 +22,6 @@ public class PrinceCard extends Card {
         return cardValue;
     }
 
-
     /**
      * By calling this method player can choose any player including themselves to discard their card
      * and draw a new card. If the discarded card is princess the player will be eliminated from the round.
@@ -30,8 +31,7 @@ public class PrinceCard extends Card {
     public void handlecard(Player playerPlayingCard) {
 
         for (Player player : round.getActivePlayers()) {
-            if (!player.isGuarded &&                // must not be guarded
-                    (this.nameOfCard == "Prince" || player != playerPlayingCard))   // Not needed? can be prince (discarding own card is allowed)
+            if (!player.isGuarded)               // must not be guarded
             {
                 availablePlayers.add(player);
             }
@@ -41,7 +41,8 @@ public class PrinceCard extends Card {
         // Print the name from the Set<Player>....
         playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
 
-        // TODO Read the input of the user
+        //Read the input of the user and return the target player
+        getTargetPlayer();
 
         // TODO Set the targetPlayer as per users choice from the list of players
 
@@ -49,19 +50,35 @@ public class PrinceCard extends Card {
             // And targetPlayer can be playerPlayingCard which is already included in availablePlayers.
             // TODO Change the println statement
 
-            if (targetPlayer.getCard().getCardName().equals("princess")) {
+            if (targetPlayer.getCard().getCardName().equals("Princess")) {
                 // Display this messages to all the players.
 
-                System.out.println("Target player has discarded a princess because of you! " +
+                System.out.println("Target player has discarded a princess because of " + playerPlayingCard + "!" +
                         "\nTarget player is now out of the game!" +
                         " \n*shakes fist angrily");
                 //we kick the player out of the round.
                 round.kickPlayer(targetPlayer);
             }
             else{
+                targetPlayer.setCurrentCard(round.pop());
                 // TODO targetPlayer needs to draw a card from the deck
 
             }
         }
+
+    }
+
+    /**
+     * reads the input of player and returns the selected player to which the card effect should be applied.
+     * @return targetPlayer the selected player
+     */
+    Player getTargetPlayer(){
+        targetPlayerName = gameboard.readResponse();
+        for (Player player : availablePlayers) {
+            if(player.getName().equals(targetPlayerName)){
+                targetPlayer = player;
+            }
+        }
+        return targetPlayer;
     }
 }
