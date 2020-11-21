548374e8c3adc81b77e2510488a0e590f5934471
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-21 21:44:02 +0100

    added createAvailablePlayers() to shorten handlecard method

--- src/card/PrinceCard.java
+++ src/card/PrinceCard.java
@@ -5,7 +5,6 @@ import game.Player;
 public class PrinceCard extends Card {
     Player targetPlayer = null;
     String targetPlayerName = "";
-    Player playerPlayingCard = null;
 
     public PrinceCard(String nameOfCard, int cardValue) {
         this.nameOfCard = nameOfCard;
@@ -29,38 +28,41 @@ public class PrinceCard extends Card {
      */
     @Override
     public void handlecard(Player playerPlayingCard) {
+        createAvailablePlayers();
 
-        for (Player player : round.getActivePlayers()) {
-            if (!player.isGuarded)               // must not be guarded and discarding own card is allowed
-            {
-                availablePlayers.add(player);
-            }
-        }
         //Display the player name from the availablePlayers so that the player can choose the name
         playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
 
         //Read the input of the user and return the target player
         getTargetPlayer();
 
-        for (Player targetPlayer : availablePlayers) {
-            // And targetPlayer can be playerPlayingCard which is already included in availablePlayers.
-            if (targetPlayer.getCard().getCardName().equals("Princess")) {
-                //we kick the player out of the round.
-                round.kickPlayer(targetPlayer);
-                //Display message to all the players    -maybe replace with communicateAll?
-                for (Player player : availablePlayers){
-                    player.message("Target player has discarded a princess because of " + playerPlayingCard + "!" +
-                            "\nTarget player is now out of the game!" +
+        //checks if card of selected player is princess  and kick the player out of the round.
+        if (targetPlayer.getCard().getCardName().equals("Princess")) {
+            round.kickPlayer(targetPlayer);
+            //Display message to all the players    -maybe replace with communicateAll?
+            for (Player player : availablePlayers){
+                player.message(targetPlayer + " has discarded a princess because of " + playerPlayingCard + "!" +
+                            "\nThe player is now out of the game!" +
                             " \n*shakes fist angrily");
-                }
-                } else {
-                //targetPlayer needs to draw a card from the deck
-                    Card currentCard = targetPlayer.getCard();
-                    targetPlayer.getPlayedCards().add(currentCard);
-                    targetPlayer.setCurrentCard(round.pop());
-                }
-        }
+            }
+            } else {
+            //targetPlayer needs to draw a card from the deck
+                Card currentCard = targetPlayer.getCard();
+                targetPlayer.getPlayedCards().add(currentCard);
+                targetPlayer.setCurrentCard(round.pop());
+            }
+    }
 
+    /**
+     * adds all players that are not guarded to the list availablePlayers
+     */
+    void createAvailablePlayers(){
+        for (Player player : round.getActivePlayers()) {
+            if (!player.isGuarded)               // must not be guarded and discarding own card is allowed
+            {
+                availablePlayers.add(player);
+            }
+        }
     }
 
     /**
