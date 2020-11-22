17126e093e297c0df40993359d046cbbd33a74cc
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-22 17:17:19 +0100

    added if(availablePlayers.size() <= 0) to cards

--- src/card/BaronCard.java
+++ src/card/BaronCard.java
@@ -30,25 +30,28 @@ public class BaronCard extends Card {
                 availablePlayers.add(player);
             }
         }
-
-        // Display the availablePlayers so that the player can choose one
-        playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
-        // Read the input of the user and return the target player
-        getTargetPlayer();
-        // compares the hand to see who has the greater cardValue
-        int targetCardValue = targetPlayer.getCard().getCardValue();
-        int playerCardValue = playerPlayingCard.getCard().getCardValue();
-
-        if (targetCardValue > playerCardValue) {
-            round.kickPlayer(playerPlayingCard);
-            //Display message to all the players
-            controller.communicate(targetPlayer + " has been eliminated.", playerPlayingCard);
-        } else if (targetCardValue < playerCardValue) {
-            round.kickPlayer(targetPlayer);
-            //Display message to all the players
-            controller.communicate(targetPlayer + " has been eliminated.", playerPlayingCard);
+        if (availablePlayers.size() <= 0) {
+            playerPlayingCard.message("There is no player to choose. Your card is discarded without effect.");
         } else {
-            playerPlayingCard.message("Both the players have same card value.");
+            // Display the availablePlayers so that the player can choose one
+            playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
+            // Read the input of the user and return the target player
+            getTargetPlayer();
+            // compares the hand to see who has the greater cardValue
+            int targetCardValue = targetPlayer.getCard().getCardValue();
+            int playerCardValue = playerPlayingCard.getCard().getCardValue();
+
+            if (targetCardValue > playerCardValue) {
+                round.kickPlayer(playerPlayingCard);
+                //Display message to all the players
+                controller.communicate(targetPlayer + " has been eliminated.", playerPlayingCard);
+            } else if (targetCardValue < playerCardValue) {
+                round.kickPlayer(targetPlayer);
+                //Display message to all the players
+                controller.communicate(targetPlayer + " has been eliminated.", playerPlayingCard);
+            } else {
+                playerPlayingCard.message("Both the players have same card value.");
+            }
         }
     }
 }
