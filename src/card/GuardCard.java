f1519b786ca035d10d8253ed42477bade7cd8161
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-22 16:50:16 +0100

    added if(availablePlayer <= 0)

--- src/card/GuardCard.java
+++ src/card/GuardCard.java
@@ -32,35 +32,38 @@ public class GuardCard extends Card{
         for (Player player : round.getActivePlayers()) {
             if (!player.isGuarded() && player != playerPlayingCard) {
                 availablePlayers.add(player);
-
             }
         }
 
-         // Display the player name from the availablePlayers so that the player can choose the name
-        playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
+        if(availablePlayers.size() <= 0){
+            playerPlayingCard.message("There is no player to choose. Your card is discarded without effect.");
+        } else {
+            // Display the player name from the availablePlayers so that the player can choose the name
+            playerPlayingCard.message("Choose one of these players: " + availablePlayers.toString());
 
-        // Read the input of the user and set to targetPlayer
-        // Set the targetPlayer as per users choice from the list of players
-        getTargetPlayer();
-
-        // Then playerPlayingCard can guess the card of the targetPlayer
-        playerPlayingCard.message("What card do you think the player has?");
-        // Read the input of the player
-        String guessCardName = round.readResponse();
-
-
-        if (guessCardName.equals(this.nameOfCard)) {
-            playerPlayingCard.message("You cannot choose the guard card, try again.");
+            // Read the input of the user and set to targetPlayer
+            // Set the targetPlayer as per users choice from the list of players
             getTargetPlayer();
 
-        }else if(guessCardName.equals(targetPlayer.getCard().getCardName())) {
-            playerPlayingCard.message("Your guess was correct!");
-            //If the guess is correct the player will be out of the round.
-            round.kickPlayer(targetPlayer);
-            //Display message to all the players
-            controller.communicateAll(targetPlayer + "is eliminated from the round.");
-        }else {
-            playerPlayingCard.message("Your guess was Incorrect.");
+            // Then playerPlayingCard can guess the card of the targetPlayer
+            playerPlayingCard.message("What card do you think the player has?");
+            // Read the input of the player
+            String guessCardName = round.readResponse();
+
+
+            if (guessCardName.equals(this.nameOfCard)) {
+                playerPlayingCard.message("You cannot choose the guard card, try again.");
+                getTargetPlayer();
+
+            } else if (guessCardName.equals(targetPlayer.getCard().getCardName())) {
+                playerPlayingCard.message("Your guess was correct!");
+                //If the guess is correct the player will be out of the round.
+                round.kickPlayer(targetPlayer);
+                //Display message to all the players
+                controller.communicateAll(targetPlayer + "is eliminated from the round.");
+            } else {
+                playerPlayingCard.message("Your guess was Incorrect.");
+            }
         }
     }
 }
--- src/card/KingCard.java
+++ src/card/KingCard.java
@@ -44,5 +44,6 @@ public class KingCard extends Card {
         targetPlayer.setCurrentCard(playerCard);
         playerPlayingCard.setCurrentCard(targetCard);
         //TODO message to the players
+
     }
 }
