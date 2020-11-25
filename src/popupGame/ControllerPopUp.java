bcdee66a16dc01577190b4969995c4d298b7489d
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-11-25 15:23:14 +0100

    create popup & edited game view

---
+++ src/popupGame/ControllerPopUp.java
@@ -0,0 +1,4 @@
+package popupGame;
+
+public class ControllerPopUp {
+}
---
+++ src/popupGame/PopUp.java
@@ -0,0 +1,16 @@
+package popupGame;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PopUp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
