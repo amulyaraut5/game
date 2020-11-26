c1911abbe6f596aa5b8baddf64680b109ea9911e
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-26 20:18:17 +0100

    added handleRuleCard that opens the rulecard pop-up

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -65,4 +65,20 @@ public class Controller  {
         //show pop-up and wait until it is dismissed
         popup.showAndWait();
     }
+
+    /**
+     * Handles the pop-up of the rule card
+     */
+    public Button ruleCardButton;
+    public void handleruleCardButton(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/popupGame/ruleCard.fxml"));
+        Stage popup = new Stage();
+        popup.setScene(new Scene(root));
+        //for pop-up:
+        popup.initModality(Modality.APPLICATION_MODAL);
+        //reminde popup-window of its "owner"/ gets the popup-window infomation
+        popup.initOwner(ruleCardButton.getScene().getWindow());
+        //show pop-up and wait until it is dismissed
+        popup.showAndWait();
+    }
 }
