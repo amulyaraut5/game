a5d82925a69c436b1c3757319eb779e6fbb63683
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-26 19:56:19 +0100

    added handleOkButton to close the popup

--- src/popupGame/ControllerPopUp.java
+++ src/popupGame/ControllerPopUp.java
@@ -1,5 +1,20 @@
 package popupGame;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class ControllerPopUp {
 
+    public Button closePopUp;
+    
+    public void handleOkButton(javafx.event.ActionEvent actionEvent) {
+        Stage window = (Stage) closePopUp.getScene().getWindow();
+        window.close();
+    }
 }
