26f08cc62c108dafe98234e163a2a1a7e9b09e2c
unterberg <unterberg@cip.ifi.lmu.de>
2020-11-28 17:59:49 +0100

    Added User Output and functionality

--- src/card/PrinceCard.java
+++ src/card/PrinceCard.java
@@ -44,6 +44,8 @@ public class PrinceCard extends Card {
             Card currentCard = targetPlayer.getCard();
             targetPlayer.getPlayedCards().add(currentCard);
             targetPlayer.setCurrentCard(round.pop());
+            controller.communicate( targetPlayer + " has to draw a new card.", targetPlayer);
+            targetPlayer.message("You have to draw a new card. Your new card is " + playerPlayingCard.getCard() + ".");
         }
         availablePlayers.clear();
     }
