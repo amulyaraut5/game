44e1d9fb07ef947b856d48228de5f7220c450753
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-11-28 13:10:23 +0100

    fill choicebox with playernames

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -37,6 +37,7 @@ public class LoginController {
         Stage stage = new Stage();
         stage.setScene(gameViewScene);
         stage.show();
+
     }
 
 
--- src/popupGame/ControllerPopUp.java
+++ src/popupGame/ControllerPopUp.java
@@ -1,15 +1,22 @@
 package popupGame;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ControllerPopUp {
+public class ControllerPopUp implements Initializable {
 
     public Button closePopUp;
     
@@ -17,4 +24,19 @@ public class ControllerPopUp {
         Stage window = (Stage) closePopUp.getScene().getWindow();
         window.close();
     }
+    String player0 = "0";
+    String player1 = "1";
+    String player2 = "2";
+    String player3 = "3";
+    ObservableList playerList = FXCollections.observableArrayList(player0, player1, player2, player3);
+    public ChoiceBox playerBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        playerBox.setItems(playerList);
+        playerBox.setValue("Choose a player");
+    }
+
+    //ObservableList playerList = FXCollections.observableArrayList("Baron", "Countess",
+    //        "Guard", "Handmaid", "King", "Priest", "Prince", "Princess");
 }
