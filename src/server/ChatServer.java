8cda74187ae175e1d7683d73120111a29057f3a2
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-11-30 14:37:29 +0100

    trying to close the serverSocket on closure of the terminal

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -62,6 +62,14 @@ public class ChatServer {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Chat server is waiting for clients to connect to port " + port + ".");
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    try {
+                        serverSocket.close();
+                        System.out.println("The server is shut down!");
+                    } catch (IOException e) { /* failed */ }
+                }
+            });
             acceptClients(serverSocket);
         } catch (IOException e) {
             System.err.println("could not connect: " + e.getMessage());
