3cb141c8b399ea17fb174f4b8ff748b5bac5cfa0
Annika Voss <vossa@cip.ifi.lmu.de>
2020-11-30 23:14:21 +0100

    deleted sysout fehler

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -85,12 +85,10 @@ public class ChatClient {
      * methods ends the client program. The reader and writer threads get interrupted and the socket is closed.
      */
     public void disconnect() {
-        System.out.println("hier ist der Fehler");
         readerThread.interrupt();
         writerThread.interrupt();
         try {
             socket.close();
-            System.out.println("hier ist der Fehler");
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
