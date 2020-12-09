848307ec57d0b8beeaa911d6fea66bab7877ced8
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-12-09 15:10:18 +0100

    added Utilities class 2

---
+++ src/game/gameObjects/Utilities.java
@@ -0,0 +1,11 @@
+package game.gameObjects;
+
+public class Utilities {
+    public enum Direction {
+        NORTH, EAST, SOUTH, WEST
+    }
+
+    public enum Rotation {
+        LEFT, RIGHT
+    }
+}
