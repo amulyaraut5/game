85d5bac0a9094ea36045192ffbaef478b8f58ae9
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-12-09 13:29:17 +0100

    Coordinate added javadoc

--- src/game/gameObjects/Coordinate.java
+++ src/game/gameObjects/Coordinate.java
@@ -1,13 +1,30 @@
 package game.gameObjects;
 
+/**
+ * A Coordinate saves the 2 dimensional x and y position of gameObjects on the map.
+ *
+ * @author simon
+ */
 public class Coordinate {
     private int x, y;
 
+    /**
+     * Constructor creates a new Coordinate with given position.
+     *
+     * @param x horizontal parameter of a position
+     * @param y vertical parameter of a position
+     */
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Methods updates the position of a coordinate to given attributes.
+     *
+     * @param x horizontal parameter of a position
+     * @param y vertical parameter of a position
+     */
     public void update(int x, int y) {
         this.x = x;
         this.y = y;
@@ -17,6 +34,9 @@ public class Coordinate {
         return x;
     }
 
+    /**
+     * @param x horizontal parameter of a position
+     */
     public void setX(int x) {
         this.x = x;
     }
@@ -25,6 +45,9 @@ public class Coordinate {
         return y;
     }
 
+    /**
+     * @param y y vertical parameter of a position
+     */
     public void setY(int y) {
         this.y = y;
     }
