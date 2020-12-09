12215be5d64bbfc9b583e6ce1d018866d053952e
unterberg <unterberg@cip.ifi.lmu.de>
2020-12-09 18:55:23 +0100

    commented activateRobots method

--- src/game/round/ActivationPhase.java
+++ src/game/round/ActivationPhase.java
@@ -10,17 +10,24 @@ public class ActivationPhase {
 
 	public ActivationPhase() {
 		// TODO - implement ActivationPhase.ActivationPhase
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
 	}
 
 	private void activateRobots() {
-		// TODO - implement ActivationPhase.activateRobots
-		throw new UnsupportedOperationException();
+		/* TODO - implement ActivationPhase.activateRobots
+		for(register=0;register<5;register++) 		--> loops five times = Register
+			flipRegister 							--> all players turn over their cards (in view?)
+			game.getPriorityList 					--> returns priorityList
+			for(Player player : priorityList)		--> oder Robots?
+				if (!playerRebooted)
+					card.activateCard(player, register)	--> robot?
+		 */
+		//throw new UnsupportedOperationException();
 	}
 
 	private void activateBoard() {
 		// TODO - implement ActivationPhase.activateBoard
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
 	}
 
 }
