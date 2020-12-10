24bd57d367ed155d1cb6f947f45e2979055be746
unterberg <unterberg@cip.ifi.lmu.de>
2020-12-10 14:59:09 +0100

    JavaDocs and method functionality

--- src/game/round/ActivationPhase.java
+++ src/game/round/ActivationPhase.java
@@ -1,9 +1,14 @@
 package game.round;
 
 import game.Player;
-import game.round.Round;
 
 import java.util.ArrayList;
+
+/**
+ * Activation Phase, where the Programming Cards and GameBoard Tiles are activated
+ *
+ * @author janau
+ */
 
 public class ActivationPhase extends Round {
 
@@ -14,25 +19,41 @@ public class ActivationPhase extends Round {
 		//throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * starts the ActivationPhase.
+	 * In every register the priority is determined and the players cards get activated
+	 * in priority order.
+	 * After each register the method for activating the board tiles ist called.
+	 */
 	public void startActivationPhase() {
-
-	}
-
-	private void activateRobots() {
-		/* TODO - implement ActivationPhase.activateRobots
+		/*
 		for(register=0;register<5;register++) 		--> loops five times = Register
-			flipRegister 							--> all players turn over their cards (in view?)
-			game.getPriorityList 					--> returns priorityList
-			for(Player player : priorityList)		--> oder Robots?
-				if (!playerRebooted)
-					card.activateCard(player, register)	--> robot?
+		flipRegister 							--> all players turn over their cards (in view?)
+		game.getPriorityList 					--> returns priorityList
+		for(Player player : priorityList)		--> oder Robots?
+		if (!playerRebooted)
+			card.activateCard(player, register)	--> robot?
 		 */
 		//throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Method that activates the board elements in their right order.
+	 */
+
 	private void activateBoard() {
 		// TODO - implement ActivationPhase.activateBoard
-		//blueConveyor.performAction();
+		/*
+		blueConveyor.performAction();
+		greenConveyor.performAction();
+		pushPanel.performAction();
+		gear.performAction();
+		boardLaser.performAction();
+		robotLaser.performAction(); TODO is there a robot laser?
+		energySpace.performAction();
+		checkPoint.performAction();
+		 */
+
 
 		//throw new UnsupportedOperationException();
 	}
