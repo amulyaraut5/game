6fd4b3cbce3b424fe1892d8bdf49f6d511577cdb
Louis Hähle <haehlel@cip.ifi.lmu.de>
2020-12-10 18:41:20 +0100

    ChatServer: attributes communicateAll hasUsers
    UserThread:
    sendMessage

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,166 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
 
+    /**
+     * list of users that gets added after every new instance of user is created
+     */
+    private static final ArrayList<User> users = new ArrayList<>(10);
+    /**
+     * port where the server is bound to listen
+     */
+    private final int port;
+    /**
+     * instance of GameController for the communication between the ChatServer and GameController
+     */
+    //private final GameController gameController = new GameController(this);
+
+    /**
+     * Constructor for the ChatServer class which initialises the port number.
+     *
+     * @param port port number where the server is bound to listen the client.
+     */
+    public Server(int port) {
+        this.port = port;
+    }
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        int port = 5444;
+
+        Server server = new Server(port);
+        server.start();
+    }
+
+    /**
+     * Getter for the Users.
+     *
+     * @return returns the list of users.
+     */
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * It opens a channel for the connection between Server and Client.
+     */
+    private void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Chat server is waiting for clients to connect to port " + port + ".");
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    try {
+                        serverSocket.close();
+                        System.out.println("The server is shut down!");
+                    } catch (IOException e) { /* failed */ }
+                }
+            });
+            acceptClients(serverSocket);
+        } catch (IOException e) {
+            System.err.println("could not connect: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method accepts the clients request and ChatServer assigns a separate thread to handle multiple clients
+     *
+     * @param serverSocket socket from which connection is to be established
+     */
+    private void acceptClients(ServerSocket serverSocket) {
+        boolean accept = true;
+        while (accept) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted the connection from address: " + clientSocket.getRemoteSocketAddress());
+                User user = new User();
+                users.add(user);
+                UserThread thread = new UserThread(clientSocket, this, user);
+                thread.start();
+            } catch (IOException e) {
+                accept = false;
+                System.out.println("Accept failed on: " + port);
+            }
+        }
+    }
+
+    /**
+     * This method sends a message to each client which is connected to the server except the sender itself.
+     *
+     * @param message the message which is to be sent.
+     * @param sender  the user who is responsible for sending the message.
+     */
+    public void communicate(String message, User sender) {
+
+        for (User user : users) {
+            if (user != sender) {
+                user.message(message);
+            }
+        }
+    }
+
+    /**
+     * Method to check if input for sending a direct message is valid.
+     * It cuts the assigns username out of the message and checks if it was entered and/or
+     * corresponds to an existing username.
+     *
+     * @param message String of user input in form '@<username> text'
+     * @param sender  user who sent the direct message
+     * @return shows if user input for direct message was valid
+     */
+    public boolean communicateDirect(String message, User sender) {
+        String userName = (message.split(" ", 2)[0]);
+        String destinationUser = userName.substring(1);
+        String messageUser = message.split(" ", 2)[1];
+        if (messageUser.length() == 0) return false;
+        for (User user : users) {
+            if (user.getName().equals(destinationUser)) {
+                user.message("[" + sender + " (direct)]: " + messageUser);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * It sends messages to all the user.
+     *
+     * @param message message to be sent.
+     */
+    public void communicateAll(String message) {
+        for (User user : users)
+            user.message(message);
+    }
+
+    /**
+     * It checks if the username is already used of another user.
+     *
+     * @param userName userName to be checked
+     * @return True if username is free, false if it´s already assigned
+     */
+    public synchronized boolean isAvailable(String userName) {
+        for (User u : users) {
+            if (u.getName().equals(userName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * This method removes a user from the list of users.
+     *
+     * @param user User to be removed
+     */
+    public void removeUser(User user) {
+        users.remove(user);
+    }
 }
--- src/server/UserThread.java
+++ src/server/UserThread.java
@@ -1,4 +1,188 @@
 package server;
 
-public class UserThread {
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Handles connection for each connected client,
+ * therefore the server is able to handle multiple clients at the same time.
+ *
+ * @author vossa,
+ */
+
+public class UserThread extends Thread {
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
+    private final User user; //Connected user, which data has to be filled in logIn()
+    private final Socket socket;
+    private final Server server;
+    private PrintWriter userOut;
+    private BufferedReader reader;
+    private boolean exit = false;
+
+    public UserThread(Socket socket, Server server, User user) {
+        this.socket = socket;
+        this.server = server;
+        this.user = user;
+
+        this.user.setThread(this);
+
+        try {
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+            OutputStream output = socket.getOutputStream();
+            userOut = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            disconnect(ex);
+        }
+    }
+
+    /**
+     * Turns a String into a date or null
+     *
+     * @param date as String
+     * @return null if String is not a valid date or the date
+     */
+    public static LocalDate turnIntoDate(String date) {
+        try {
+            return LocalDate.parse(date, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+
+    /**
+     * The method runs a loop of reading messages from the user and sending them to all other users.
+     * The user disconnects by typing "bye".
+     */
+    @Override
+    public void run() {
+
+        //before each method call it is checked if run() should be exited.
+        if (!exit) logIn();
+        if (!exit) welcome();
+
+        String clientMessage = "";
+        String serverMessage;
+        try {
+            while (!exit && !clientMessage.equals("bye")) {
+                clientMessage = reader.readLine();
+
+                Pattern gamePattern = Pattern.compile("^#+");
+                Matcher gameMatcher = gamePattern.matcher(clientMessage);
+
+                Pattern directPattern = Pattern.compile("^@+");
+                Matcher directMatcher = directPattern.matcher(clientMessage);
+
+                 if (directMatcher.lookingAt()) {
+                    if (!server.communicateDirect(clientMessage, user)) {
+                        user.message("Your request does not correspond to the required format. Please try again. @<name> <message>");
+                    }
+                } else if (clientMessage.equals("userList")) {
+                    String userList = "userList: ";
+                    for (User user : server.getUsers()) {
+                        userList += user.getName();
+                        userList += " ";
+                    }
+                    user.message(userList);
+                } else {
+                    serverMessage = "[" + user + "]: " + clientMessage;
+                    server.communicate(serverMessage, user);
+                }
+            }
+        } catch (IOException ex) {
+            if (!exit) disconnect(ex);
+        }
+        if (!exit) disconnect();
+    }
+
+    /**
+     * prints a message for specific user
+     *
+     * @param message the message to be sent
+     */
+    public void sendMessage(String message) {
+        if (!message.isBlank()) userOut.println(message);
+    }
+
+    /**
+     * The user is asked to enter a name to log in and to enter the date where he last dated.
+     * If the name already exists in the list of assigned usernames, the user is asked to try again.
+     */
+    private void logIn() {
+        while (!exit) {
+            try {
+                String message = reader.readLine();
+                if (message != null) {
+                    String userName = message.substring(0, message.indexOf(" "));
+                    String dateText = message.substring(message.indexOf(" ") + 1);
+
+                    if (turnIntoDate(dateText) == null) {
+                        sendMessage("#login " + "Please check your Date! (dd.mm.yyyy)");
+                    } else if (!server.isAvailable(userName)) {
+                        sendMessage("#login " + "This username is already taken!");
+                    } else {
+                        user.setName(userName);
+                        sendMessage("#login " + "successful");
+                        return;
+                    }
+                }
+            } catch (IOException ex) {
+                if (!exit) disconnect(ex);
+            }
+        }
+    }
+
+
+    /**
+     * Sends welcome message to the user and notifies all other users.
+     */
+    private void welcome() {
+        sendMessage("Thank you! Welcome " + user + "!");
+        sendMessage("""
+                Type: 'bye' to leave the room.\s
+                      '@<name>' to send a direct message.\s
+                      '#help' to list all commands.\s
+                      '#create' to play the LoveLetter game.""");
+        server.communicate(user + " joined the room.", user);
+    }
+
+    /**
+     * The connection is closed and other users get notified that the user left.
+     */
+    private void disconnect() {
+        sendMessage("Bye " + user);
+        server.removeUser(user);
+        server.communicate(user + " left the room.", user);
+        System.out.println("Closed the connection with address:   " + socket.getRemoteSocketAddress());
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Method is called if an Exception occurs during connection.
+     * The connection is tried to close and other users get notified that the user left.
+     *
+     * @param ex Exception which occurred
+     */
+    private void disconnect(Exception ex) {
+        exit = true;
+        System.err.println("Error in UserThread with address " + socket.getRemoteSocketAddress() + ": " + ex.getMessage());
+        server.removeUser(user);
+        server.communicate(user + " left the room.", user);
+        System.out.println("Closed the connection with address:   " + socket.getRemoteSocketAddress());
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
