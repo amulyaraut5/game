5e7839442e83e28221d2233044e31ed892f55fa3
Louis Hähle <haehlel@cip.ifi.lmu.de>
2020-12-12 13:45:38 +0100

    GameView

---
+++ src/client/view/GameView.java
@@ -0,0 +1,29 @@
+package client.view;
+
+import client.view.login.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameView extends Application {
+
+    public static void main(String[] args){ launch(args);}
+
+    @Override
+    public void start(Stage gameStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GameView.fxml"));
+        Parent root = loader.load();
+        GameViewController gameController = loader.getController();
+
+        gameStage.setTitle("Robo Rally");
+        gameStage.setScene(new Scene(root));
+        gameStage.show();
+        gameController.setStage(gameStage);
+
+
+    }
+}
