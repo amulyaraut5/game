f8deb7f28877f3923d15880abe192b178e17066c
unterberg <unterberg@cip.ifi.lmu.de>
2020-12-12 13:44:15 +0100

    main class

---
+++ src/main/java/game/main.java
@@ -0,0 +1,15 @@
+package game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
