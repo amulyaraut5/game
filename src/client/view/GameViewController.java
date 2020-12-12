fa49f04ef23f069520e8174d08ff0db572d60492
Louis Hähle <haehlel@cip.ifi.lmu.de>
2020-12-12 12:22:56 +0100

    ChatServer: attributes communicateAll hasUsers
    UserThread:
    sendMessage

--- src/client/view/GameViewController.java
+++ src/client/view/GameViewController.java
@@ -2,10 +2,21 @@ package client.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
 public class GameViewController {
 
     @FXML
+    public Button submitButton;
+    @FXML
+    public TextArea userArea;
+    @FXML
+    public TextArea chatTextArea;
+    @FXML
+    public TextArea chatWindow;
 
 
 
@@ -13,7 +24,7 @@ public class GameViewController {
         String message = chatTextArea.getText();
         if(!message.isBlank()){
             chatWindow.appendText("[You]: " + message + "\n");
-            client.sentUserInput(message);
+            //client.sentUserInput(message);
         }
         chatTextArea.clear();
     }
