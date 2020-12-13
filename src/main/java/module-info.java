50a8ec85a2b0677ee20e0bfc4b731309efaef101
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-12-13 10:59:06 +0100

    added instructions to modile info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module roborally {
         requires javafx.controls;
         requires javafx.fxml;
     requires com.google.gson;
-
+    exports client to javafx.graphics;
     opens client.view to javafx.fxml;
         exports client.view;
         }
