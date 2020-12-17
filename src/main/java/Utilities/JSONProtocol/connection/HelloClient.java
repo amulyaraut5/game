258eaef614369e58052c43a2b89faefad04d2f86
Annika Voss <vossa@cip.ifi.lmu.de>
2020-12-17 16:35:08 +0100

    added HelloClient

--- src/main/java/Utilities/JSONProtocol/connection/HelloClient.java
+++ src/main/java/Utilities/JSONProtocol/connection/HelloClient.java
@@ -1,4 +1,27 @@
 package Utilities.JSONProtocol.connection;
 
-public class HelloClient {
+import Utilities.JSONProtocol.JSONMessage;
+
+public class HelloClient extends JSONMessage {
+
+    private class MessageBody{
+        public double protocol;
+    }
+
+    MessageBody messageBody = new MessageBody();
+
+    public HelloClient(double protocol){
+        setType("HelloClient");
+        messageBody.protocol = protocol;
+    }
+
+    @Override
+    public void clientMessage() {
+
+    }
+
+    @Override
+    public void serverMessage() {
+
+    }
 }
