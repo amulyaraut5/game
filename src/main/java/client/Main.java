2514a3aa3200ad68d7f3c5871d01616f23163e2b
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2020-12-20 19:05:51 +0100

    MenuView added

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,7 +1,7 @@
 package client;
 
-import client.view.LoginController;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,25 +11,67 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    private static Stage menuStage;
+    private static Stage gameStage;
+    private static Scene menuScene;
+    private static Scene loginScene;
+    private static Scene gameScene;
 
-	@Override
-	public void start(Stage loginStage) throws IOException {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/loginView.fxml"));
-		Parent root = loader.load();
-		LoginController loginController = loader.getController();
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-		loginStage.setTitle("Roborally");
-		loginStage.setResizable(false);
-		loginStage.setScene(new Scene(root));
-		loginStage.show();
-		loginController.setStage(loginStage);
+    @Override
+    public void start(Stage menuStage) throws IOException {
+        this.menuStage = menuStage;
 
-		loginStage.setOnCloseRequest(event -> {
-			loginController.close();
-			loginStage.close();
-		});
-	}
+        Platform.runLater(() -> {
+            constructScenes();
+            constructGameStage();
+            constructMenuStage();
+        });
+        constructGameStage();
+        constructMenuStage();
+        menuStage.show();
+    }
+
+    public void constructScenes() {
+        Parent login = null;
+        Parent game = null;
+        Parent menu = null;
+        try {
+            menu = new FXMLLoader(getClass().getResource("/menuView.fxml")).load();
+            login = new FXMLLoader(getClass().getResource("/loginView.fxml")).load();
+            game = new FXMLLoader(getClass().getResource("/gameView.fxml")).load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        menuScene = new Scene(menu);
+        loginScene = new Scene(login);
+        gameScene = new Scene(game);
+    }
+
+    public void constructMenuStage() {
+        menuStage.setTitle("RoboRally - Menu");
+        menuStage.setResizable(false);
+        menuStage.setScene(loginScene);
+
+        menuStage.setOnCloseRequest(event -> {
+            menuStage.close();
+        });
+
+    }
+
+    public void constructGameStage() {
+        if (gameStage == null) {
+            gameStage = new Stage();
+        }
+        gameStage.setTitle("RoboRally - Game");
+        gameStage.setResizable(false);
+        gameStage.setScene(gameScene);
+
+        gameStage.setOnCloseRequest(event -> {
+            gameStage.close();
+        });
+    }
 }
