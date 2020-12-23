952690f70fb201ee3566d2c8f67865a482a51823
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2020-12-23 08:42:36 +0100

    javadoc GameViewController

--- src/main/java/client/view/GameViewController.java
+++ src/main/java/client/view/GameViewController.java
@@ -11,20 +11,49 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class GameViewController {
-
+    /**
+     *
+     */
     private Stage stage;
+    /**
+     *
+     */
     public Client client;
+
+    /**
+     *
+     */
     @FXML
     public Button submitButton;
+
+    /**
+     *
+     */
     @FXML
     public TextArea userArea;
+
+    /**
+     *
+     */
     @FXML
     public TextField chatTextArea;
+
+    /**
+     *
+     */
     @FXML
     public TextArea chatWindow;
 
+    /**
+     *
+     */
     private Main main;
 
+
+    /**
+     *
+     * @param actionEvent
+     */
     //TODO
     public void sendChatMessage(ActionEvent actionEvent) {
         String message = chatTextArea.getText();
@@ -36,23 +65,41 @@ public class GameViewController {
         chatTextArea.clear();
     }
 
-
+    /**
+     *
+     * @param s
+     */
     public void setStage(Stage s) {
         this.stage = s;
     }
 
+    /**
+     *
+     */
     public void close() {
         //client.disconnect(); TODO disconnect client on closure of window
     }
 
+    /**
+     *
+     * @param client
+     */
     public void setClient(Client client) {
         this.client = client;
     }
 
+    /**
+     *
+     * @param messageBody
+     */
     public void setTextArea(String messageBody) {
         chatWindow.appendText(messageBody + "\n");
     }
 
+    /**
+     *
+     * @param main
+     */
     public void setMain(Main main) {
         this.main = main;
     }
