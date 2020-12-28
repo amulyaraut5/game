61aa3aad38f3bd6b328fd4794bef0e3351e3776e
Louis Hähle <haehlel@cip.ifi.lmu.de>
2020-12-28 14:41:44 +0100

    Added LobbyView and LobbyController

---
+++ src/main/java/client/view/LobbyController.java
@@ -0,0 +1,23 @@
+package client.view;
+import client.Main;
+import client.model.Client;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import utilities.JSONProtocol.JSONMessage;
+import utilities.JSONProtocol.connection.HelloServer;
+import utilities.JSONProtocol.lobby.PlayerValues;
+import utilities.Utilities;
+
+import static utilities.Utilities.PORT;
+
+
+
+public class LobbyController {
+
+}
---
+++ src/main/java/client/view/LobbyView.java
@@ -0,0 +1,13 @@
+package client.view;
+import client.Main;
+import client.model.Client;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LobbyView {
+
+}
