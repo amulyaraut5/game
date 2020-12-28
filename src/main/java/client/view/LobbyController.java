fbb7ec7ebe080dadea8a90f39c0c324e63ad7b6e
Louis Hähle <haehlel@cip.ifi.lmu.de>
2020-12-28 19:00:10 +0100

    Changes in LobbyController

--- src/main/java/client/view/LobbyController.java
+++ src/main/java/client/view/LobbyController.java
@@ -10,12 +10,41 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import utilities.JSONProtocol.JSONMessage;
+import utilities.JSONProtocol.body.SetStatus;
 import utilities.Utilities;
 
 import static utilities.Utilities.PORT;
 
 
 
+
 public class LobbyController {
 
+    private Stage lobbyStage;
+
+    private Client client;
+
+    private String userName;
+
+    @FXML
+    private CheckBox readyCheckbox;
+
+
+    @FXML
+    private void checkBoxAction() {
+
+        JSONMessage msg = new JSONMessage(new SetStatus(readyCheckbox.isSelected()));
+        client.sendMessage(msg);
+
+
+
+    }
+
+    public void setStage(Stage lobbyStage) {
+        this.lobbyStage = lobbyStage;
+    }
+
+    public void close() {
+        //TODO
+    }
 }
--- src/main/java/client/view/LobbyView.java
+++ src/main/java/client/view/LobbyView.java
@@ -1,13 +1,40 @@
 package client.view;
 import client.Main;
 import client.model.Client;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LobbyView {
+import java.io.IOException;
+
+public class LobbyView extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage lobbyStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/lobbyView.fxml"));
+        Parent root = loader.load();
+        LobbyController lobbyController = loader.getController();
+
+        lobbyStage.setTitle("Roborally");
+        lobbyStage.setScene(new Scene(root));
+        lobbyStage.show();
+        lobbyController.setStage(lobbyStage);
+
+        lobbyStage.setOnCloseRequest(event -> {
+            lobbyController.close();
+            lobbyStage.close();
+        });
+    }
+
 
 }
