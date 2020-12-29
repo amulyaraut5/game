4395760692a4c2751312ec0c327d86fc39166e7e
Annika Voss <vossa@cip.ifi.lmu.de>
2020-12-29 21:28:09 +0100

    updated cases in Tile class

--- src/main/java/game/gameObjects/tiles/Tile.java
+++ src/main/java/game/gameObjects/tiles/Tile.java
@@ -15,6 +15,7 @@ public class Tile {
 
 
 
+
 	/**
 	 * Constructor for tiles with one attribute.
 	 * @param attribute1 first attribute
@@ -68,58 +69,95 @@ public class Tile {
 		switch(tileID){
 			case 00: tile = new Tile(new Empty());
 
-			case 01: tile = new Tile(new Antenna());
+			case 111: tile = new Tile(new Antenna());
 					// draw();
 				// TODO should be changed
 				// Belt constructor has 2 attributes.
 
-			case 11: tile = new Tile(new Belt(Utilities.upLeft, 1));
-			case 12: tile = new Tile(new Belt(Utilities.upRight, 1));
-			case 13: tile = new Tile(new Belt(Utilities.downLeft, 1));
-			case 14: tile = new Tile(new Belt(Utilities.downRight, 1));
-			case 15: tile = new Tile(new Belt(Utilities.Orientation.UP, 1));
-			case 16: tile = new Tile(new Belt(Utilities.Orientation.DOWN, 1));
-			case 17: tile = new Tile(new Belt(Utilities.Orientation.LEFT, 1));
-			case 18: tile = new Tile(new Belt(Utilities.Orientation.RIGHT, 1));
-
-
+			//Green Conveyor Belts
+			case 01: tile = new Tile(new Belt(Utilities.Orientation.UP, 1));
+			case 02: tile = new Tile(new Belt(Utilities.Orientation.DOWN, 1));
+			case 03: tile = new Tile(new Belt(Utilities.Orientation.LEFT, 1));
+			case 04: tile = new Tile(new Belt(Utilities.Orientation.RIGHT, 1));
 			// draw();
-			case 21: tile = new Tile(new RotatingBelt(Utilities.upLeft, false));
-			case 22: tile = new Tile(new RotatingBelt(Utilities.upRight, false));
-			case 23: tile = new Tile(new RotatingBelt(Utilities.downLeft, false));
-			case 24: tile = new Tile(new RotatingBelt(Utilities.downRight, false));
-			// draw();
-			case 31: tile = new Tile(new Laser(Utilities.Orientation.DOWN, 1));
-			case 32: tile = new Tile(new Laser(Utilities.Orientation.UP,1));
-			case 33: tile = new Tile(new Laser(Utilities.Orientation.LEFT,1));
-			case 34: tile = new Tile(new Laser(Utilities.Orientation.RIGHT,1));
-			// draw();
-			case 41: tile = new Tile(new EnergySpace(1));
-			// draw();
-			case 51: tile = new Tile(new Pit());
 
-			case 61: tile = new Tile(new Gear(Utilities.Orientation.LEFT));
-			case 62: tile = new Tile(new Gear(Utilities.Orientation.RIGHT));
+			//Blue Conveyor Belts
+			case 11: tile = new Tile(new Belt(Utilities.Orientation.UP, 2));
+			case 12: tile = new Tile(new Belt(Utilities.Orientation.DOWN, 2));
+			case 13: tile = new Tile(new Belt(Utilities.Orientation.LEFT, 2));
+			case 14: tile = new Tile(new Belt(Utilities.Orientation.RIGHT, 2));
+				// draw();
+
+			//Green Rotating Conveyor Belts
+			case 21: tile = new Tile(new RotatingBelt(Utilities.upLeft, false, 1));
+			case 22: tile = new Tile(new RotatingBelt(Utilities.upRight, false, 1));
+			case 23: tile = new Tile(new RotatingBelt(Utilities.downLeft, false, 1));
+			case 24: tile = new Tile(new RotatingBelt(Utilities.downRight, false, 1));
+
+			case 25: tile = new Tile(new RotatingBelt(Utilities.upLeft, true, 1));
+			case 26: tile = new Tile(new RotatingBelt(Utilities.upRight, true, 1));
+			case 27: tile = new Tile(new RotatingBelt(Utilities.downLeft, true, 1));
+			case 28: tile = new Tile(new RotatingBelt(Utilities.downRight, true, 1));
 			// draw();
-			case 711: tile = new Tile(new PushPanel(Utilities.Orientation.DOWN,1));
-			case 712: tile = new Tile(new PushPanel(Utilities.Orientation.UP,1));
-			case 713: tile = new Tile(new PushPanel(Utilities.Orientation.LEFT,1));
-			case 714: tile = new Tile(new PushPanel(Utilities.Orientation.RIGHT,1));
 
-			case 721: tile = new Tile(new PushPanel(Utilities.Orientation.DOWN,2));
-			case 722: tile = new Tile(new PushPanel(Utilities.Orientation.UP,2));
-			case 723: tile = new Tile(new PushPanel(Utilities.Orientation.LEFT,2));
-			case 724: tile = new Tile(new PushPanel(Utilities.Orientation.RIGHT,2));
+			//Blue Rotating Conveyor Belts
+			case 31: tile = new Tile(new RotatingBelt(Utilities.upLeft, false, 1));
+			case 32: tile = new Tile(new RotatingBelt(Utilities.upRight, false, 1));
+			case 33: tile = new Tile(new RotatingBelt(Utilities.downLeft, false, 1));
+			case 34: tile = new Tile(new RotatingBelt(Utilities.downRight, false, 1));
 
-			case 731: tile = new Tile(new PushPanel(Utilities.Orientation.DOWN,3));
-			case 732: tile = new Tile(new PushPanel(Utilities.Orientation.UP,3));
-			case 733: tile = new Tile(new PushPanel(Utilities.Orientation.LEFT,3));
-			case 734: tile = new Tile(new PushPanel(Utilities.Orientation.RIGHT,3));
+			case 35: tile = new Tile(new RotatingBelt(Utilities.upLeft, true, 1));
+			case 36: tile = new Tile(new RotatingBelt(Utilities.upRight, true, 1));
+			case 37: tile = new Tile(new RotatingBelt(Utilities.downLeft, true, 1));
+			case 38: tile = new Tile(new RotatingBelt(Utilities.downRight, true, 1));
+			// draw();
 
-			case 81: tile = new Tile(new Wall(Utilities.Orientation.DOWN));
-			case 82: tile = new Tile(new Wall(Utilities.Orientation.UP));
-			case 83: tile = new Tile(new Wall(Utilities.Orientation.LEFT));
-			case 84: tile = new Tile(new Wall(Utilities.Orientation.RIGHT));
+			//Push Panels
+			case 40: tile = new Tile(new PushPanel(Utilities.Orientation.DOWN, new int[]{2, 4}));
+			case 41: tile = new Tile(new PushPanel(Utilities.Orientation.UP, new int[]{2,4}));
+			case 42: tile = new Tile(new PushPanel(Utilities.Orientation.LEFT ,new int[]{2,4}));
+			case 43: tile = new Tile(new PushPanel(Utilities.Orientation.RIGHT, new int[]{2,4}));
+
+			case 44: tile = new Tile(new PushPanel(Utilities.Orientation.DOWN, new int[]{1, 3, 5}));
+			case 45: tile = new Tile(new PushPanel(Utilities.Orientation.UP, new int[]{1, 3, 5}));
+			case 46: tile = new Tile(new PushPanel(Utilities.Orientation.LEFT, new int[]{1, 3, 5}));
+			case 47: tile = new Tile(new PushPanel(Utilities.Orientation.RIGHT, new int[]{1, 3, 5}));
+
+			//Gears
+			case 51: tile = new Tile(new Gear(Utilities.Orientation.LEFT));
+			case 52: tile = new Tile(new Gear(Utilities.Orientation.RIGHT));
+			// draw();
+
+			//Board Lasers
+			case 61: tile = new Tile(new Laser(Utilities.Orientation.DOWN, 1));
+			case 62: tile = new Tile(new Laser(Utilities.Orientation.UP,1));
+			case 63: tile = new Tile(new Laser(Utilities.Orientation.LEFT,1));
+			case 64: tile = new Tile(new Laser(Utilities.Orientation.RIGHT,1));
+
+			case 65: tile = new Tile(new Laser(Utilities.Orientation.DOWN, 2));
+			case 66: tile = new Tile(new Laser(Utilities.Orientation.UP,2));
+			case 67: tile = new Tile(new Laser(Utilities.Orientation.LEFT,2));
+			case 68: tile = new Tile(new Laser(Utilities.Orientation.RIGHT,2));
+
+			case 71: tile = new Tile(new Laser(Utilities.Orientation.DOWN, 3));
+			case 72: tile = new Tile(new Laser(Utilities.Orientation.UP,3));
+			case 73: tile = new Tile(new Laser(Utilities.Orientation.LEFT,3));
+			case 74: tile = new Tile(new Laser(Utilities.Orientation.RIGHT,3));
+			// draw();
+
+			//Energy Spaces
+			case 81: tile = new Tile(new EnergySpace(0));
+			case 82: tile = new Tile(new EnergySpace(1));
+			// draw();
+
+			//Pits
+			case 90: tile = new Tile(new Pit());
+
+			//Walls
+			case 91: tile = new Tile(new Wall(Utilities.Orientation.DOWN));
+			case 92: tile = new Tile(new Wall(Utilities.Orientation.UP));
+			case 93: tile = new Tile(new Wall(Utilities.Orientation.LEFT));
+			case 94: tile = new Tile(new Wall(Utilities.Orientation.RIGHT));
 
 			// Tile with two walls
 
