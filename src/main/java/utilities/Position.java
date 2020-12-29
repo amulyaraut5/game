10aa12668039067f7722a62362cacb4410edf802
unterberg <unterberg@cip.ifi.lmu.de>
2020-12-29 12:16:28 +0100

    added Position class for position of tiles or robots

---
+++ src/main/java/utilities/Position.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class Position {
+    private int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
