f83812dc9fdd1df5de296c77b746f6aaaf5b5058
Annika Voss <vossa@cip.ifi.lmu.de>
2021-01-03 19:55:08 +0100

    added AgainCard and AgainAction

---
+++ src/main/java/game/gameActions/AgainAction.java
@@ -0,0 +1,17 @@
+package game.gameActions;
+
+import game.Player;
+import utilities.Utilities.Orientation;
+
+/**
+ * Repeats the programming in the players previous register.
+ * @author annika
+ */
+public class AgainAction extends Action{
+
+    @Override
+    public void doAction(Orientation orientation, Player player) {
+        //TODO if previous register was a damage card, draw card from deck, play that card this register.
+            player.setCurrentAction(player.getLastAction());
+    }
+}
---
+++ src/main/java/game/gameObjects/cards/AgainCard.java
@@ -0,0 +1,29 @@
+package game.gameObjects.cards;
+
+import game.gameActions.AgainAction;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * @author annika
+ */
+public class AgainCard extends ProgrammingCard{
+    private static final Logger logger = LogManager.getLogger();
+
+    AgainCard(){
+        super("Again");
+        super.setAction(new AgainAction());
+        drawCardImage();
+        logger.info("Repeat the programming in previous register.");
+    }
+
+    @Override
+    public ImageView drawCardImage() {
+        var stream = getClass().getResourceAsStream("/programming-cards/again-card.png");
+        var image = new Image(stream);
+        return new ImageView(image);
+    }
+}
