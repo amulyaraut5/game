a3f2e8dbe4b5f6e4d6f1e4f3a5a6a6306f0ef5e5
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-04 08:45:19 +0100

    javadoc LobbyController

--- src/main/java/client/view/LobbyController.java
+++ src/main/java/client/view/LobbyController.java
@@ -27,35 +27,104 @@ public class LobbyController extends Controller {
 
 
     @FXML
-    public TextArea lobbyTextAreaChat;
+    private TextArea lobbyTextAreaChat;
     @FXML
-    public TextField lobbyTextFieldChat;
+    private TextField lobbyTextFieldChat;
 
     @FXML
-    public CheckBox readyCheckbox;
-
-    public TextArea joinedUsersTextArea;
-
-    public TextArea readyUsersTextArea;
-
-    public ImageView robot1ImageView;
-    public ImageView robot2ImageView;
-    public ImageView robot3ImageView;
-    public ImageView robot4ImageView;
-    public ImageView robot5ImageView;
-    public ImageView robot6ImageView;
-    public Label robot1Label;
-    public Label robot2Label;
-    public Label robot3Label;
-    public Label robot4Label;
-    public Label robot5Label;
-    public Label robot6Label;
+    private CheckBox readyCheckbox;
+    /**
+     * ImageView of the first possible user
+     */
+    @FXML
+    private ImageView robot1ImageView;
+    /**
+     * ImageView of the second possible user
+     */
+    @FXML
+    private ImageView robot2ImageView;
+    /**
+     * ImageView of the third possible user
+     */
+    @FXML
+    private ImageView robot3ImageView;
+    /**
+     * ImageView of the fourth possible user
+     */
+    @FXML
+    private ImageView robot4ImageView;
+    /**
+     * ImageView of the fifth possible user
+     */
+    @FXML
+    private ImageView robot5ImageView;
+    /**
+     * ImageView of the sixth possible user
+     */
+    @FXML
+    private ImageView robot6ImageView;
+    /**
+     * Label of the first possible user
+     */
+    @FXML
+    private Label robot1Label;
+    /**
+     * Label of the second possible user
+     */
+    @FXML
+    private Label robot2Label;
+    /**
+     * Label of the third possible user
+     */
+    @FXML
+    private Label robot3Label;
+    /**
+     * Label of the fourth possible user
+     */
+    @FXML
+    private Label robot4Label;
+    /**
+     * Label of the fifth possible user
+     */
+    @FXML
+    private Label robot5Label;
+    /**
+     * Label of the sixth possible user
+     */
+    @FXML
+    private Label robot6Label;
+    /**
+     * the currentImageView that gets filled if the next user
+     * joins (all robotImageViews before  have already been filled)
+     */
     private ImageView currentImageView;
+    /**
+     * the Label that gets filled if the next user
+     * joins (all Labels before have already been filled)
+     */
     private Label currentLabel;
 
-    public ChoiceBox<String> directChoiceBox;
+    /**
+     * the choiceBox where the user can choose if the
+     * message should be a direct message or who should be
+     * the receiver of the message
+     */
+    @FXML
+    private ChoiceBox<String> directChoiceBox;
+
+    /**
+     * In robotImageViews the different ImageViews that can be assigned
+     * are stored together
+     */
     private ArrayList<ImageView> robotImageViews = new ArrayList<>();
+    /**
+     * In robotLabels the different Labels that can be assigned
+     * are stored together
+     */
     private ArrayList<Label> robotLabels = new ArrayList<>();
+    /**
+     * In robotIcons
+     */
     private ArrayList<RobotIcon> robotIcons = new ArrayList<>();
 
     public void initialize() {
@@ -164,14 +233,26 @@ public class LobbyController extends Controller {
         directChoiceBox.getSelectionModel().select(0);
     }
 
+    /**
+     * This private class is a data structure to easily connect the different information
+     * that are necessary to assign one user to a specific place in the lobby with his image, name etc.
+     */
     private class RobotIcon {
-        String userName;
-        int userID;
-        int position;
-        int figure;
-        ImageView robotImageView;
-        Label labelOfUser;
+        private String userName;
+        private int userID;
+        private int position;
+        private int figure;
+        private ImageView robotImageView;
+        private Label labelOfUser;
 
+        /**
+         * the constructor of RobotIcon where one RobotIcon can be created with the
+         * values of one player and the image of the figure he choosed
+         * @param position
+         * @param playerAdded
+         * @param imageViewPuffer
+         * @param labelPuffer
+         */
         public RobotIcon(int position, PlayerAdded playerAdded, ImageView imageViewPuffer, Label labelPuffer) {
             this.position = position;
             this.userID = playerAdded.getId();
