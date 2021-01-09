1b4538e8518094f4be23e62be18cf5bb7bc60273
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-09 14:09:27 +0100

    GameTimer is now a Thread and in run() the Thread "sleeps" and then a function of ProgrammingPhase will get called

--- src/main/java/game/round/GameTimer.java
+++ src/main/java/game/round/GameTimer.java
@@ -2,29 +2,42 @@ package game.round;
 
 import game.Game;
 import game.Player;
+import server.Server;
+import utilities.JSONProtocol.JSONMessage;
+import utilities.JSONProtocol.body.TimerStarted;
 
 /**
  * if one player is ready with picking cards, he creates an instance of timer (if timer isn´t running yet)
  */
 
-public class GameTimer {
-
-	public GameTimer(Player callingPlayer){
-		super();
+public class GameTimer extends Thread {
+	private Player callingPlayer;
+	private ProgrammingPhase programmingPhase;
+	public GameTimer(Player callingPlayer, ProgrammingPhase programmingPhase){
 		//start 30 sec timer
-		startTimer();
+		this.callingPlayer = callingPlayer;
+		this.programmingPhase = programmingPhase;
 		//after 30 sec call for every player in notReadyPlayers of ProgrammingPhase the method timerunout
 		// for (Player player : notReadyPlayers){
 		//	if (player.name != callingPlayer.name){
 		//		player.timeRunOut();}}
+
 
 	}
 
 	/**
 	 * a method that starts a 30 sec timer
 	 */
-	public void startTimer(){
-
+	@Override
+	public void run(){
+		JSONMessage timerStarted = new JSONMessage(new TimerStarted());
+		Server.getInstance().communicateAll(timerStarted);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		programmingPhase.setNotFinished(true);
 	}
 
 }
