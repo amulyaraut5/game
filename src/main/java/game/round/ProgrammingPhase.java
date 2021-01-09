d494fdbc4e9f28ec7d5d19b6d1bfbc258526c80c
unterberg <unterberg@cip.ifi.lmu.de>
2021-01-09 11:38:33 +0100

    icluded Switch case for selectedCard in ProgrammingPhase and implemented ShuffleCoding Protocol

--- src/main/java/game/round/ProgrammingPhase.java
+++ src/main/java/game/round/ProgrammingPhase.java
@@ -2,6 +2,10 @@ package game.round;
 
 import game.Player;
 import game.gameObjects.cards.Card;
+import game.gameObjects.cards.programming.Again;
+import game.gameObjects.cards.programming.MoveI;
+import game.gameObjects.cards.programming.MoveII;
+import game.gameObjects.cards.programming.MoveIII;
 import game.gameObjects.decks.ProgrammingDeck;
 import server.Server;
 import utilities.JSONProtocol.JSONMessage;
@@ -48,14 +52,36 @@ public class ProgrammingPhase {
     // (weil soweit ich das sehe ist das ja im gleichen Zeitschritt)
     //TODO server has to call this method if he gets the protocol cardselected
     private void cardWasSelected(Player player, SelectCard selectCard) {
-        player.setRegisterAndCards(selectCard.getRegister(), selectCard.getCard());
-        if (player.getRegisterAndCards().size() == 5) {
-            onePlayerFinished(player);
+        String cardType = selectCard.getCard();
+        Card chosenCard = null;
+        switch (cardType) {
+            case "again":
+                chosenCard = new Again();
+                break;
+            case "moveI":
+                chosenCard = new MoveI();
+                break;
+            case "moveII":
+                chosenCard = new MoveII();
+                break;
+            case "moveIII":
+                chosenCard = new MoveIII();
+                break;
+            //TODO other types of cards
+            case "null":
+                chosenCard = null; // player can empty a register
+                break;
         }
-    }
+
+            player.setRegisterAndCards(selectCard.getRegister(), chosenCard);
+            if (player.getRegisterAndCards().size() == 5) {
+                onePlayerFinished(player);
+            }
+        }
+
 
     /**
-     * every player can look at their programming cards
+     * every player can look at their programming cards -> Turn cards around after everyone's cards were dealt?
      */
     private void showCards() {
     }
@@ -96,10 +122,12 @@ public class ProgrammingPhase {
             if (currentDeck.size() >= 9) {
                 availableProgrammingCards = currentDeck.drawCards(9);
             } else {
-                //TODO send ShuffleCoding
                 availableProgrammingCards = currentDeck.drawCards(currentDeck.size());
                 player.reuseDiscardedDeck();
                 availableProgrammingCards.addAll(player.getDrawProgrammingDeck().drawCards(9 - currentDeck.size()));
+
+                JSONMessage shuffleCoding = new JSONMessage(new ShuffleCoding(player.getId()));
+                player.message(shuffleCoding);
             }
             player.setDrawnProgrammingCards(availableProgrammingCards);
             JSONMessage yourCards = new JSONMessage(new YourCards(availableProgrammingCards));
