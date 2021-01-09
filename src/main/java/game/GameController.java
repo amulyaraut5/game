bfd5e2a7504ea66047a9e62f2ad9202021ba216b
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-09 14:08:05 +0100

    GameController has two BlockingQueues and a method to fill them, in the metehod th playerQueue and selectCardQueue of ProgrammingPhase will get changed

--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -1,19 +1,37 @@
 package game;
 
+import utilities.JSONProtocol.JSONMessage;
+import utilities.JSONProtocol.body.SelectCard;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * TODO
  */
 
-public class GameController{
+public class GameController  {
 
-	private GameController instance;
+	private static GameController instance;
+	private BlockingQueue<Player> playerQueue = new LinkedBlockingQueue<>();
+	private BlockingQueue<SelectCard> selectCardQueue = new LinkedBlockingQueue<>();
 
-	public GameController getInstance() {
+
+	public  static GameController getInstance() {
 		if (instance == null) {
 			instance = new GameController();
 		}
 		return instance;
 	}
+	private JSONMessage jsonMessage;
+	public synchronized void setJsonMessage(JSONMessage jsonMessage){
+		this.jsonMessage = jsonMessage;
+	}
 
+	public void sendToProgrammingPhase(Player player,SelectCard selectCard) throws InterruptedException {
+		playerQueue.put(player);
+		selectCardQueue.put(selectCard);
+	}
 
 }
