efd55ff7f30d334f50ed3c142c2445dd52762e91
Amulya Raut <raut@cip.ifi.lmu.de>
2021-01-10 23:18:12 +0100

    added dependency in pom file for media and added soundtrack for games

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,13 @@ module roborally {
     requires com.google.gson;
     requires org.apache.logging.log4j;
     requires java.desktop;
+    requires javafx.media;
 
     exports client.view to javafx.graphics;
     exports client to javafx.graphics;
     exports game.gameObjects.tiles to com.google.gson;
     exports game.gameObjects.maps to com.google.gson;
+    exports utilities to javafx.graphics;
     opens game.gameObjects.maps;
     opens client.view to javafx.fxml;
     opens client to javafx.fxml;
