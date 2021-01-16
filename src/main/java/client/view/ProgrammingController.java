d369037d6c72392df23acfb2711422af7a8e1c87
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-16 23:52:58 +0100

    createMethod creates a StackPane with a lot of EventHandlers and related method that handle drag and dropping

--- src/main/java/client/view/ProgrammingController.java
+++ src/main/java/client/view/ProgrammingController.java
@@ -85,6 +85,95 @@ public class ProgrammingController {
 
         event.consume();
 
+    }*/
+
+
+    private StackPane createNewPane(){
+        StackPane pane = new StackPane();
+
+        pane.setPrefHeight(heightHBox);
+        pane.setPrefWidth(widthHBox-20);
+        pane.setStyle("-fx-border-color: #d100ea;");
+        pane.setStyle("-fx-background-color: #FFFFFF;");
+
+        pane.setOnDragOver(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent dragEvent) {
+                mouseDragOver(dragEvent, pane);
+            }
+        });
+        pane.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent dragEvent) {
+                mouseDragDropped(dragEvent, pane);
+            }
+        });
+        pane.setOnDragExited(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(DragEvent dragEvent) {
+                pane.setStyle("-fx-border-color: #C6C6C6;");
+            }
+        });
+
+        return pane;
     }
 
+
+    private void setOnDragDetected(MouseEvent mouseEvent, ImageView imageView) {
+        Dragboard db = imageView.startDragAndDrop(TransferMode.ANY);
+        ClipboardContent content = new ClipboardContent();
+        content.putImage(imageView.getImage());
+
+        db.setContent(content);
+        imageView.setImage(new Image(getClass().getResource("/programming-cards/backside-card.png").toString()));
+        mouseEvent.consume();
+    }
+
+
+
+    void addImage(Image i, StackPane pane){
+        imageView = new ImageView();
+        imageView.setFitWidth(widthHBox-20);
+        imageView.setFitHeight(heightHBox);
+        imageView.setImage(i);
+        imageView.setOnDragDetected(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                setOnDragDetected(mouseEvent, imageView);
+            }
+        });
+        pane.getChildren().add(imageView);
+
+
+    }
+
+    private void mouseDragDropped(DragEvent event, StackPane pane) {
+        Dragboard db = event.getDragboard();
+        boolean success = false;
+        if (db.hasImage()) {
+            success = true;
+            if (!pane.getChildren().isEmpty()) {
+                pane.getChildren().remove(0);
+            }
+            Image img = db.getImage();
+
+            addImage(img, pane);
+
+        }
+        event.setDropCompleted(success);
+        event.consume();
+    }
+
+    private  void mouseDragOver(DragEvent event, StackPane pane) {
+
+
+        pane.setStyle("-fx-border-color: red;"
+                + "-fx-border-width: 5;"
+                + "-fx-background-color: #C6C6C6;"
+                + "-fx-border-style: solid;");
+        event.acceptTransferModes(TransferMode.COPY);
+        event.consume();
+    }
 }
+
+
