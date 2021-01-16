62531ea89183b540a5175dc713505573f7c2e1eb
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-16 23:52:14 +0100

    now there are two hBoxes in ProgrammingController and if first is full of cards the second gets filled. Also StackPanes instead of imageViews are created for each card in ArrayList

--- src/main/java/client/view/ProgrammingController.java
+++ src/main/java/client/view/ProgrammingController.java
@@ -10,6 +10,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import utilities.JSONProtocol.body.PlayerAdded;
 
 import java.util.ArrayList;
 
@@ -19,42 +21,62 @@ public class ProgrammingController {
     //public ImageView imageView;
     //@FXML
     //public ImageView programCard;
-
+    private ImageView imageView;
     public AnchorPane programmingPhasePane;
-    public HBox hBox;
+    @FXML
+    private HBox hBox1;
+
+    public HBox hBox2;
+    private double widthHBox;
+    private double heightHBox;
 
     public void initialize(){ //TODO method that gets called when cards were dealt
+        widthHBox = hBox1.getPrefWidth()/5;
+        heightHBox = hBox1.getPrefHeight();
+        hBox1.setSpacing(20);
+        hBox2.setSpacing(20);
         ArrayList<String> cardList = new ArrayList<>(); //HARDCODED
         cardList.add("Again");
         cardList.add("MoveI");
         cardList.add("MoveII");
         cardList.add("MoveIII");
         cardList.add("UTurn");
+        cardList.add("Again");
+        cardList.add("MoveI");
+        cardList.add("MoveII");
+        cardList.add("MoveIII");
         for (String card : cardList){
-            ImageView programCard = new ImageView();
-            programCard.setImage(new Image("/programming-cards/" + card +"-card.png"));
-            programCard.setFitWidth(90);
-            programCard.setFitHeight(116);
-            programCard.setOnDragDetected(new EventHandler<MouseEvent>() {
+            StackPane pane = createNewPane();
+            addImage(new Image("/programming-cards/" + card +"-card.png"), pane);
+            //ImageView programCard = new ImageView();
+            //programCard.setImage(new Image("/programming-cards/" + card +"-card.png"));
+            //programCard.setFitWidth(90);
+            //programCard.setFitHeight(116);
+            /*pane.setOnDragDetected(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
                     setOnDragDetected(mouseEvent, programCard);
                 }
-            });
-            hBox.getChildren().add(programCard);
+            });*/
+            if(!(hBox1.getChildren().size()>=5)){
+                hBox1.getChildren().add(pane);
+            } else {
+                hBox2.getChildren().add(pane);
+            }
+
         }
 
     }
 
 
 
-
+/*
     @FXML
     private void setOnDragDetected(Event event, ImageView imageView) {
         Dragboard db = imageView.startDragAndDrop(TransferMode.ANY);
         //Dragboard db = programCard1.startDragAndDrop(TransferMode.ANY);
         //Dragboard db = source.startDragAndDrop(TransferMode.ANY);
-        /* Put a string on a dragboard */
+        /* Put a string on a dragboard
         ClipboardContent content = new ClipboardContent();
 
         //content.put(new DataFormat(cardName),imageView.getImage());
