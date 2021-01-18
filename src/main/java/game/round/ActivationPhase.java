6666b9db70e55e325ffa2d2b563391eb2047f7cf
Annika Voss <vossa@cip.ifi.lmu.de>
2021-01-18 21:13:13 +0100

    moved Card functionality to ActivationPhase in handleCard()

--- src/main/java/game/round/ActivationPhase.java
+++ src/main/java/game/round/ActivationPhase.java
@@ -1,6 +1,11 @@
 package game.round;
 
 import game.Player;
+import game.gameActions.*;
+import game.gameObjects.cards.Card;
+import game.gameObjects.cards.damage.Spam;
+import game.gameObjects.cards.damage.Trojan;
+import game.gameObjects.cards.damage.Worm;
 import game.gameObjects.maps.Map;
 import game.gameObjects.robot.Robot;
 import game.gameObjects.tiles.Attribute;
@@ -8,6 +13,8 @@ import game.gameObjects.tiles.Belt;
 import game.gameObjects.tiles.RotatingBelt;
 import game.gameObjects.tiles.Wall;
 import game.gameObjects.tiles.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import utilities.Coordinate;
 import utilities.JSONProtocol.body.CurrentCards;
 import utilities.JSONProtocol.body.Error;
@@ -32,10 +39,9 @@ import static java.lang.StrictMath.abs;
 
 public class ActivationPhase extends Phase {
 
+    private static final Logger logger = LogManager.getLogger();
+
     // TODO when we transfer StartBoard: private ArrayList<Player> priorityList;
-
-    private Antenna antenna = new Antenna();
-
 
     /**
      * saves the Player ID and the card for the current register
@@ -102,8 +108,6 @@ public class ActivationPhase extends Phase {
     /**
      * Method that activates the board elements in their right order.
      */
-
-
     private void activateBoard() {
         // TODO - implement ActivationPhase.activateBoard
         gameMap = game.getMap();
@@ -233,6 +237,116 @@ public class ActivationPhase extends Phase {
     public void moveOne(Player player, Orientation orientation) {
         player.getRobot().move(1, orientation);
         server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+    }
+
+
+    public void handleCard(CardType cardType, Player player) {
+        Orientation orientation = player.getRobot().getOrientation();
+
+        switch (cardType) {
+            case MoveI -> {
+                moveOne(player, orientation);
+                logger.info(player.getName() + "moved one Tile.");
+            }
+            case MoveII -> {
+                moveOne(player, orientation);
+                moveOne(player, orientation);
+                logger.info(player.getName() + "moved two Tiles.");
+            }
+            case MoveIII -> {
+                moveOne(player, orientation);
+                moveOne(player, orientation);
+                moveOne(player, orientation);
+                logger.info(player.getName() + "moved three Tiles.");
+            }
+            case  TurnLeft -> {
+                RotateRobot rotateRobot = new RotateRobot((Orientation.LEFT));
+                rotateRobot.doAction(Orientation.LEFT, player);
+                server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+                logger.info(player.getName() + "turned left.");
+            }
+            case TurnRight -> {
+                RotateRobot rotateRobot = new RotateRobot((Orientation.RIGHT));
+                rotateRobot.doAction(Orientation.RIGHT, player);
+                server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+                logger.info(player.getName() + "turned right.");
+            }
+            case UTurn -> {
+                RotateRobot rotateRobot = new RotateRobot((Orientation.RIGHT));
+                rotateRobot.doAction(Orientation.RIGHT, player);
+                rotateRobot.doAction(Orientation.RIGHT, player);
+                server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+                logger.info(player.getName() + "performed U-Turn");
+            }
+            case BackUp -> {
+                new MoveRobotBack().doAction(orientation, player);
+                server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+                logger.info(player.getName() + "moved back.");
+            }
+            case PowerUp -> {
+                player.setEnergyCubes(player.getEnergyCubes() + 1);
+                logger.info(player.getName() + "got one EnergyCube.");
+            }
+            case Again -> {
+                new AgainAction().doAction(orientation, player);
+                server.communicateAll(MapConverter.convertCoordinate(player, player.getRobot().getPosition()));
+            }
+            case Spam -> {
+                Spam spam = new Spam();
+                //Add spam card back into the spam deck
+                game.getSpamDeck().getDeck().add(spam);
+                //remove top card from programming deck
+                Card topCard = player.getDrawProgrammingDeck().pop();
+
+                //exchange spam card and new programming card in the current register
+                int spamIndex = player.getRegisterCards().indexOf(spam);
+                player.getRegisterCards().remove(spam);
+                player.getRegisterCards().set(spamIndex, topCard);
+
+                //Play the new register card
+                handleCard(topCard.getName(), player);
+            }
+            case Worm -> {
+                Worm worm = new Worm();
+                //Reboot the robot.
+                new RebootAction().doAction(orientation, player);
+                //Add worm card back into the worm deck
+                game.getWormDeck().getDeck().add(worm);
+            }
+            case Virus -> {
+                int robotX = player.getRobot().getPosition().getX();
+                int robotY = player.getRobot().getPosition().getY();
+                ArrayList<Player> allPlayers = game.getPlayers();
+
+                for (Player otherPlayer : allPlayers) {
+                    int otherRobotX = otherPlayer.getRobot().getPosition().getX();
+                    int otherRobotY = otherPlayer.getRobot().getPosition().getY();
+
+                    if (otherPlayer != player && (otherRobotX <= robotX + 6 || otherRobotY <= robotY + 6)) {
+                        Card virusCard = game.getVirusDeck().pop();
+                        otherPlayer.getDiscardedProgrammingDeck().getDeck().add(virusCard);
+                    }
+                }
+            }
+            case Trojan -> {
+                Trojan trojan = new Trojan();
+                game.getTrojanHorseDeck().getDeck().add(trojan);
+                Card topCard = player.getDrawProgrammingDeck().pop();
+
+                //exchange spam card and new programming card in the current register
+                int trojanIndex = player.getRegisterCards().indexOf(trojan);
+                player.getRegisterCards().remove(trojan);
+                player.getRegisterCards().set(trojanIndex, topCard);
+
+                //Draw two spam cards
+                for (int i = 0; i < 2; i++) {
+                    Card spamCard = game.getSpamDeck().pop();
+                    player.getDiscardedProgrammingDeck().getDeck().add(spamCard);
+                }
+                //Play the new register card
+                handleCard(topCard.getName(), player);
+            }
+        }
     }
 
 
