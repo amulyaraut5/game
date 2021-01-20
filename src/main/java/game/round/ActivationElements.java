ebc96ef42045ff382bd509b7596abe4ae85bb811
Amulya Raut <raut@cip.ifi.lmu.de>
2021-01-21 00:03:29 +0100

    javadocs and updated ControlPoint codes

--- src/main/java/game/round/ActivationElements.java
+++ src/main/java/game/round/ActivationElements.java
@@ -5,32 +5,39 @@ import game.Game;
 import game.Player;
 import game.gameActions.MoveRobot;
 import game.gameActions.RebootAction;
+import game.gameActions.RotateRobot;
 import game.gameObjects.maps.Map;
 import game.gameObjects.tiles.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import server.Server;
 import utilities.Coordinate;
 import utilities.JSONProtocol.JSONBody;
 import utilities.JSONProtocol.body.*;
 import utilities.enums.AttributeType;
 import utilities.enums.Orientation;
+import utilities.enums.Rotation;
 
 import java.util.ArrayList;
 
 public class ActivationElements {
     private static final Logger logger = LogManager.getLogger();
+    protected Server server = Server.getInstance();
     private Game game = Game.getInstance();
     private ArrayList<Player> playerList = game.getPlayers();
     private Map map = game.getMap();
     private ActivationPhase activationPhase;
 
-
+    /**
+     * Player landing on pit needs to reboot.
+     * Player is suspended from the current round and must discard all programming cards.
+     * Reboot Protocol is sent to the player.
+     */
     public void activatePit() {
         for (Coordinate coordinate :map.getPitCoordinates()) {
             for(Player player: playerList){
                 if (player.getRobot().getCoordinate() == coordinate) {
                     new RebootAction().doAction(player.getRobot().getOrientation(), player);
-
                     JSONBody jsonBody = new Reboot(player.getID());
                     player.message(jsonBody);
                 }
@@ -38,12 +45,18 @@ public class ActivationElements {
         }
     }
 
+    /**
+     * Gears rotate robots resting on them 90 degrees in the direction of the arrows.
+     * Red gears rotate left, and green gears rotate right.
+     * PlayerTurning Protocol is sent to all players.
+     */
     public void activateGear(){
         for (Coordinate coordinate : map.getGearCoordinates()) {
             for(Player player: playerList){
                 if (player.getRobot().getCoordinate() == coordinate) {
-                    //TODO Change Rotation to Orientation or vice verse
+                    for(Attribute a:  map.getTile(coordinate.getX(),coordinate.getY()).getAttributes()){
 
+                    }
                 }
             }
         }
@@ -53,45 +66,35 @@ public class ActivationElements {
      * Checkpoint is the final destination of the game and player wins the game as
      * soon as the player has reached all the checkpoints in chronological order.
      * The player gets the checkpoint token.
-     *
+     * CheckPointReached and GameWon Protocol are sent.
      */
 
     public void activateControlPoint(){
+        outerLoop:
         for(Coordinate coordinate: map.getControlPointCoordinates()){
-            Tile tile = map.getTile(coordinate);
-            for(Attribute a : tile.getAttributes()){
-                int count = ((game.gameObjects.tiles.Laser) a).getCount();
-
+            for(Attribute a : map.getTile(coordinate).getAttributes()){
+                int checkPointID = ((game.gameObjects.tiles.ControlPoint) a).getCount();
                 for(Player player: playerList){
                     if (player.getRobot().getCoordinate() == coordinate){
-
-                        JSONBody jsonBody = new CheckpointReached(player.getID(),count);
-                        player.message(jsonBody);
-
-                        if (game.getNoOfCheckPoints() == 1) {
-                            JSONBody jsonBody1 = new GameWon(player.getID());
-                            player.message(jsonBody1);
-                            // TODO End the game:
-                        }
-                        else if (game.getNoOfCheckPoints() != 1) {
-                            if (player.getCheckPointCounter() > count) {
-                                logger.info("Checkpoint already reached");
-                                // Maybe inform all the clients and users
-
-                            } else if (player.getCheckPointCounter() < 1) {
-                                logger.info(" 1st Checkpoint not reached.");
-
-                            } else if (player.getCheckPointCounter() == 1) {
-                                int checkPoint = player.getCheckPointCounter();
-                                checkPoint++;
-                                player.setCheckPointCounter(checkPoint);
-
-                                if ((game.getNoOfCheckPoints() == 2) && (count == 2)) {
-                                    JSONBody jsonBody1 = new GameWon(player.getID());
-                                    player.message(jsonBody1);
-                                    // TODO End the game:
-                                }
+                        if (player.getCheckPointCounter() == (checkPointID-1)) {
+                            if(checkPointID < game.getNoOfCheckPoints()){
+                               int checkPoint = player.getCheckPointCounter();
+                               checkPoint++;
+                               player.setCheckPointCounter(checkPoint);
+                               JSONBody jsonBody = new CheckpointReached(player.getID(),checkPointID);
+                               player.message(jsonBody);
                             }
+                            else if (checkPointID == game.getNoOfCheckPoints()){
+                               JSONBody jsonBody1 = new GameWon(player.getID());
+                               server.communicateAll(jsonBody1);
+                               // TODO End the Game
+                                break outerLoop;
+                            }
+                        }
+                        else if (player.getCheckPointCounter() > checkPointID) {
+                            logger.info("CheckPoint Already Reached");
+                        }else {
+                            logger.info("You need to go CheckPoint " + (player.getCheckPointCounter() + 1) + " first");
                         }
                     }
                 }
@@ -104,6 +107,7 @@ public class ActivationElements {
      * panel faces. They activate only in the register that corresponds to the number on them. For
      * example, if you end register two on a push panel labeled “2, 4” you will be pushed. If you end
      * register three on the same push panel, you won’t be pushed.
+     * Movement Protocol is sent to the players.
      */
 
     public void activatePushPanel(){
@@ -112,12 +116,32 @@ public class ActivationElements {
             for(Player player: playerList){
                 if (player.getRobot().getCoordinate() == coordinate) {
                     for(Attribute a : tile.getAttributes()){
-                        for(int i : ((game.gameObjects.tiles.PushPanel) a).getRegisters()){
+                        for(int i : ((PushPanel) a).getRegisters()){
                             if( i == player.getCurrentRegister()){
-                                new MoveRobot().doAction(((game.gameObjects.tiles.Laser) a).getOrientation(), player);
+                                new MoveRobot().doAction(((PushPanel) a).getOrientation(), player);
                             }
                         }
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * Whenever the player finds in this tile, player gets the energy token.
+     * EnergyProtocol is sent to the player.
+     */
+
+    public void activateEnergySpace(){
+        for(Coordinate coordinate: map.getEnergySpaces()){
+            for(Player player: playerList){
+                if (player.getRobot().getCoordinate() == coordinate) {
+                    int energy = player.getEnergyCubes();
+                    energy += energy;
+                    player.setEnergyCubes(energy);
+                    JSONBody jsonBody = new Energy(player.getID(), player.getEnergyCubes());
+                    player.message(jsonBody);
+
                 }
             }
         }
@@ -288,27 +312,6 @@ public class ActivationElements {
         }
     }
 
-    /**
-     * Whenever the player finds in this tile, player gets the energy token.
-     */
-    // No more usage
 
-    public void activateEnergySpace(){
-        for(Coordinate coordinate: map.getEnergySpaces()){
-            for(Player player: playerList){
-                if (player.getRobot().getCoordinate() == coordinate) {
-
-                    int energy = player.getEnergyCubes();
-                    energy += energy;
-                    player.setEnergyCubes(energy);
-                    // Todo Decrease the energy cube number
-
-                    JSONBody jsonBody = new Energy(player.getID(), player.getEnergyCubes());
-                    player.message(jsonBody);
-
-                }
-            }
-        }
-    }
 
 }
