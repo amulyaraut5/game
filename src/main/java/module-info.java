586f5df47e9939d6c74b8e87db88bd8a728c00b3
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2021-01-21 22:39:58 +0100

    #63 tried out JFoenix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,14 +5,16 @@ module roborally {
     requires org.apache.logging.log4j;
     requires java.desktop;
     requires javafx.media;
+    requires com.jfoenix;
 
+    exports client;
     exports client.view to javafx.graphics;
-    exports client to javafx.graphics;
     exports game.gameObjects.tiles to com.google.gson;
     exports game.gameObjects.maps to com.google.gson;
     exports utilities to javafx.graphics;
     exports game.gameObjects.cards.programming to com.google.gson;
     exports game.gameActions to com.google.gson;
+
     opens game.gameObjects.maps;
     opens client.view to javafx.fxml;
     opens client to javafx.fxml;
