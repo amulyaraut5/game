fbdbb53ee16eb9416f54cc375e0c90ba92357c06
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-23 22:25:07 +0100

    pickDamage gets handled, 4 buttons for different damageCards and it gets saved if player has selected as he should

--- src/main/java/client/view/ActivationController.java
+++ src/main/java/client/view/ActivationController.java
@@ -71,4 +71,46 @@ public class ActivationController extends Controller {
     public  void setInfoLabel(String text){
         infoLabel.setText(text);
     }
+
+    public void pickDamage(PickDamage pickDamage){
+        playCardAnchorPane.setVisible(false);
+        selectDamageAnchorPane.setVisible(true);
+        setInfoLabel("You have to pick " + pickDamage.getCount() + " damage cards");
+        this.pickDamage = pickDamage.getCount();
+    }
+
+    @FXML
+    private void spamCard(ActionEvent actionEvent) {
+        checkDamageReady(CardType.Spam);
+    }
+
+    @FXML
+    private void trojanCard(ActionEvent actionEvent) {
+        checkDamageReady(CardType.Trojan);
+    }
+
+    @FXML
+    private void virusCard(ActionEvent actionEvent) {
+        checkDamageReady(CardType.Virus);
+    }
+
+    @FXML
+    private void wormCard(ActionEvent actionEvent) {
+        checkDamageReady(CardType.Worm);
+    }
+
+    private void checkDamageReady(CardType card){
+        pickedDamageCards.add(card);
+        ImageView selectedDamageCard = new ImageView(new Image(getClass().getResource("/cards/programming/" + card + "-card.png").toString()));
+        selectedDamageCard.setFitHeight(150);
+        selectedDamageCard.setFitWidth(70);
+        selectedDamageHBox.getChildren().add(selectedDamageCard);
+        if(pickedDamageCards.size()==pickDamage){
+            client.sendMessage(new SelectDamage(pickedDamageCards));
+            pickDamage = 0;
+            pickedDamageCards.clear();
+            playCardAnchorPane.setVisible(true);
+            selectDamageAnchorPane.setVisible(false);
+        }
+    }
 }
