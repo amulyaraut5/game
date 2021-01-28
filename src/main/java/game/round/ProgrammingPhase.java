73f12696690f117ac319d213ee9f958c5cd72159
unterberg <unterberg@cip.ifi.lmu.de>
2021-01-28 19:30:36 +0100

    fixed bug with cards not being recognized

--- src/main/java/game/round/ProgrammingPhase.java
+++ src/main/java/game/round/ProgrammingPhase.java
@@ -92,7 +92,11 @@ public class ProgrammingPhase extends Phase {
 
         if (chosenCard != null) {
             //put the card in the register
-            if (player.getRegisterCards().contains(chosenCard)) {
+            ArrayList<CardType> cardTypes = new ArrayList<>();
+            for (Card card : player.getDrawnProgrammingCards()) {
+                cardTypes.add(card.getName());
+            }
+            if (cardTypes.contains(type)) {
                 player.setRegisterCards(selectCard.getRegister(), chosenCard);
             } else {
                 server.communicateDirect(new Error("This is not a valid card."), player.getID());
@@ -103,7 +107,6 @@ public class ProgrammingPhase extends Phase {
 
             //Here the card with the same cardType enum as the chosen card is removed from the hand cards
             CardType chosenCardType = chosenCard.getName();
-            ArrayList<CardType> cardTypes = new ArrayList<>();
             for (Card card : player.getDrawnProgrammingCards()) {
                 cardTypes.add(card.getName());
             }
@@ -114,7 +117,7 @@ public class ProgrammingPhase extends Phase {
                     break;
                 }
             }
-        //if a card was removed, remove the card from the register and put it in the hand cards
+            //if a card was removed, remove the card from the register and put it in the hand cards
         } else {
             Card removeCard = player.getRegisterCard(selectCard.getRegister());
             player.getDrawnProgrammingCards().add(removeCard);
