dafd2d892efc80a1c2c39099d72add0eecc6b2f9
Simon Dürrmeier <duerrmeier@cip.ifi.lmu.de>
2021-01-30 23:17:00 +0100

    Used for laser animation methods from LaserAction, removed calculateRobotEndCoordinate and calculateEndCoordinate

--- src/main/java/client/view/GameController.java
+++ src/main/java/client/view/GameController.java
@@ -6,7 +6,7 @@ import game.gameObjects.robot.Robot;
 import game.gameObjects.tiles.Attribute;
 import game.gameObjects.tiles.Empty;
 import game.gameObjects.tiles.Laser;
-import game.gameObjects.tiles.Wall;
+import game.round.LaserAction;
 import javafx.animation.FadeTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.SequentialTransition;
@@ -49,12 +49,11 @@ public class GameController extends Controller implements Updatable {
     private static final Logger logger = LogManager.getLogger();
 
     private final Group[][] fields = new Group[Utilities.MAP_WIDTH][Utilities.MAP_HEIGHT];
+    private ArrayList<Player> players = client.getPlayers();
+    private ArrayList<Player> rebootingPlayers = new ArrayList<>();
     private ArrayList<Coordinate> path = new ArrayList<>();
     private HashMap<Player, ImageView> robotTokens = new HashMap<>();
     private Map map;
-    ArrayList<Player> players = client.getPlayers();
-    ArrayList<Player> rebootingPlayers = new ArrayList<>();
-
     private PlayerMatController playerMatController;
     private ConstructionController constructionController;
     private ProgrammingController programmingController;
@@ -103,13 +102,10 @@ public class GameController extends Controller implements Updatable {
     private int currentRound = 1;
     private boolean first = true;
 
-
     private int countSpamCards = 38;
     private int countTrojanCards = 12;
     private int countWormCards = 6;
     private int countVirusCards = 18;
-
-
 
     @FXML
     public void initialize() {
@@ -145,9 +141,6 @@ public class GameController extends Controller implements Updatable {
 
         this.soundHandler = new SoundHandler();
     }
-
-
-
 
     public void attachChatPane(Pane chat) {
         chat.setPrefWidth(chatPane.getPrefWidth());
@@ -322,7 +315,7 @@ public class GameController extends Controller implements Updatable {
                     }
 
                     animationPane.getChildren().add(imageView);
-                    Coordinate newPos = calculateEndCoordinate(orientation, c, players);
+                    Coordinate newPos = LaserAction.calculateLaserEnd(c, orientation, map, players);
                     imageView.setX(c.getX() * Utilities.FIELD_SIZE);
                     imageView.setY(c.getY() * Utilities.FIELD_SIZE);
 
@@ -371,7 +364,7 @@ public class GameController extends Controller implements Updatable {
                 case LEFT, RIGHT -> imageView.setRotate(270);
             }
 
-            Coordinate newPos = calculateRobotEndCoordinate(orientation, robotPosition, players);
+            Coordinate newPos = LaserAction.calculateLaserEnd(robotPosition, orientation, map, players);
             imageView.setX(robotPosition.getX() * Utilities.FIELD_SIZE);
             imageView.setY(robotPosition.getY() * Utilities.FIELD_SIZE);
 
@@ -396,131 +389,6 @@ public class GameController extends Controller implements Updatable {
 
     }
 
-    /**
-     * It calculates the last possible coordinate where laser terminates for the robot.
-     *
-     * @param orientation Direction at which robot is facing
-     * @param position    Position of robot in board.
-     * @param players     active players list from the game
-     * @return Coordinate
-     */
-
-    private Coordinate calculateRobotEndCoordinate(Orientation orientation, Coordinate position, ArrayList<Player> players) {
-        position = position.clone();
-        Coordinate step = orientation.toVector();
-
-        outerLoop:
-        while (true) {
-            position.add(step);
-            if (position.isOutOfBound()) {
-                //logger.info("Laser Out of Bound");
-                break outerLoop;
-            } else {
-                for (Attribute b : map.getTile(position.getX(), position.getY()).getAttributes()) {
-                    if (b.getType() != AttributeType.Wall && b.getType() != AttributeType.Antenna && b.getType() != AttributeType.Laser
-                            && b.getType() != AttributeType.ControlPoint) {
-                        path.add(position.clone());
-                        break;
-                    } else if (b.getType() == AttributeType.ControlPoint && b.getType() == AttributeType.Laser) {
-                        path.add(position.clone());
-                        break outerLoop;
-                    } else if (b.getType() == AttributeType.Laser) {
-                        if (((Laser) b).getOrientation() == orientation) {
-                            break outerLoop;
-                        } else if (((Laser) b).getOrientation() == orientation.getOpposite()) {
-                            path.add(position.clone());
-                            break outerLoop;
-                        } else if (((Laser) b).getOrientation() != orientation) {
-                            path.add(position.clone());
-                            break;
-                        }
-                    } else if (b.getType() == AttributeType.Wall) {
-                        if (((Wall) b).getOrientation() == orientation) {
-                            path.add(position.clone());
-                            break outerLoop;
-                        } else if (((Wall) b).getOrientation() == orientation.getOpposite()) {
-                            break outerLoop;
-                        } else if (((Wall) b).getOrientation() != orientation) {
-                            path.add(position.clone());
-                            break;
-                        }
-                    } else if (b.getType() == AttributeType.Antenna) break outerLoop;
-                }
-            }
-        }
-        if (path.size() == 0) {
-            //logger.info("Nowhere to fire");
-            return position;
-        } else {
-            for (Coordinate coordinate : path) {
-                for (Player player : players) {
-                    if (coordinate.equals(player.getRobot().getCoordinate())) return coordinate;
-                }
-            }
-            return path.get(path.size() - 1);
-        }
-    }
-
-    /**
-     * It calculates the last possible coordinate where laser terminates for the board lasers.
-     *
-     * @param orientation Direction at which laser is facing
-     * @param position    Position of laser in board
-     * @param players     Possible player who could be affected
-     * @return Coordinate
-     */
-    private Coordinate calculateEndCoordinate(Orientation orientation, Coordinate position, ArrayList<Player> players) {
-
-        path.add(position);
-        position = position.clone();
-        Coordinate step = orientation.toVector();
-
-        outerLoop:
-        while (true) {
-            position.add(step);
-            if (position.isOutOfBound()) {
-                //logger.info("Laser Out of Bound");
-                break outerLoop;
-            } else {
-                for (Attribute b : map.getTile(position.getX(), position.getY()).getAttributes()) {
-                    if (b.getType() != AttributeType.Wall && b.getType() != AttributeType.Antenna && b.getType() != AttributeType.Laser
-                            && b.getType() != AttributeType.ControlPoint) {
-                        path.add(position.clone());
-                        break;
-                    } else if (b.getType() == AttributeType.ControlPoint && b.getType() == AttributeType.Laser) {
-                        path.add(position.clone());
-                        break outerLoop;
-                    } else if (b.getType() == AttributeType.Laser) {
-                        if (((Laser) b).getOrientation() == orientation) {
-                            break outerLoop;
-                        } else if (((Laser) b).getOrientation() == orientation.getOpposite()) {
-                            path.add(position.clone());
-                            break outerLoop;
-                        } else if (((Laser) b).getOrientation() != orientation) {
-                            path.add(position.clone());
-                            break;
-                        }
-                    } else if (b.getType() == AttributeType.Wall) {
-                        if (((Wall) b).getOrientation() == orientation) {
-                            path.add(position.clone());
-                            break outerLoop;
-                        } else if (((Wall) b).getOrientation() == orientation.getOpposite()) {
-                            break outerLoop;
-                        } else if (((Wall) b).getOrientation() != orientation) {
-                            path.add(position.clone());
-                            break;
-                        }
-                    } else if (b.getType() == AttributeType.Antenna) break outerLoop;
-                }
-            }
-        }
-        for (Coordinate coordinate : path) {
-            for (Player player : players) {
-                if (coordinate.equals(player.getRobot().getCoordinate())) return coordinate;
-            }
-        }
-        return path.get(path.size() - 1);
-    }
 
     public ProgrammingController getProgrammingController() {
         return programmingController;
@@ -659,27 +527,19 @@ public class GameController extends Controller implements Updatable {
         othersController.removePlayer(player);
     }
 
-    public void handleDamageCount(ArrayList<CardType> cardList){
-        for(CardType cardType: cardList){
-           handleDamageCount(cardType);
+    public void handleDamageCount(ArrayList<CardType> cardList) {
+        for (CardType cardType : cardList) {
+            handleDamageCount(cardType);
         }
     }
 
-    public void handleDamageCount(CardType cardType){
-            switch (cardType) {
-                case Spam -> {
-                    countSpamCards--;
-                }
-                case Trojan -> {
-                    countTrojanCards--;
-                }
-                case Worm -> {
-                    countWormCards--;
-                }
-                case Virus -> {
-                    countVirusCards--;
-                }
-            }
+    public void handleDamageCount(CardType cardType) {
+        switch (cardType) {
+            case Spam -> countSpamCards--;
+            case Trojan -> countTrojanCards--;
+            case Worm -> countWormCards--;
+            case Virus -> countVirusCards--;
+        }
     }
 
     public int getCountSpamCards() {
@@ -753,7 +613,7 @@ public class GameController extends Controller implements Updatable {
                 handleShooting(players);
                 handleRobotShooting(players);
             }
-            case Reboot->{
+            case Reboot -> {
                 Reboot reboot = (Reboot) message.getBody();
                 rebootingPlayers.add(client.getPlayerFromID(reboot.getPlayerID()));
                 players.remove(client.getPlayerFromID(reboot.getPlayerID()));
