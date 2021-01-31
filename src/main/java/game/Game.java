587e5efde3dd64c4775b87d6ca028135b57a3ece
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-01-31 11:23:07 +0100

    handle exception, in case of invalid call of #damage x in programming phase

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -179,8 +179,10 @@ public class Game {
             }
             case "#damage" -> {
                 Robot robot = userToPlayer(user).getRobot();
-                if (cheatInfo.length == 0)
+                if (cheatInfo.length == 0 )
                     server.communicateDirect(new Error("your cheat is invalid!"), user.getID());
+                else if(getActivationPhase()== null)
+                    server.communicateDirect(new Error("your cheat is invalid in this phase"), user.getID());
                 else activationPhase.drawDamage(spamDeck, userToPlayer(user), Integer.parseInt(cheatInfo[0]));
             }
             case "#autoPlay" -> {
