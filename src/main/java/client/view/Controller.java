65db6002f7ef3c43bbb1e3260461e3ba39dfa368
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-02-02 21:54:04 +0100

    switching counts of damage cards and related methods to Controller + setter and getter and decreasing count,...

--- src/main/java/client/view/Controller.java
+++ src/main/java/client/view/Controller.java
@@ -4,7 +4,11 @@ import client.ViewManager;
 import client.model.Client;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DataFormat;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import utilities.enums.CardType;
+
+import java.util.ArrayList;
 
 /**
  * Abstract super class of all view-controller
@@ -12,6 +16,7 @@ import utilities.enums.CardType;
  * @author simon, sarah
  */
 public abstract class Controller {
+    private static final Logger logger = LogManager.getLogger();
 
     protected static DataFormat cardFormat = new DataFormat("programmingCard");
     private static int positionRegister;
@@ -32,11 +37,65 @@ public abstract class Controller {
         Controller.wasFormerRegister = wasFormerRegister;
     }
 
+    private static int countSpamCards = 38;
+    private static int countTrojanCards = 12;
+    private static int countWormCards = 6;
+    private static int countVirusCards = 18;
+    protected int getCountSpamCards() {
+        return countSpamCards;
+    }
+
+    public void setCountSpamCards(int countSpamCards) {
+        if((countSpamCards<0)) this.countSpamCards = 0;
+        else this.countSpamCards = countSpamCards;
+    }
+
+    public int getCountTrojanCards() {
+        return countTrojanCards;
+    }
+    public void setCountTrojanCards(int countTrojanCards) {
+        if((countTrojanCards<0)) this.countTrojanCards = 0;
+        else this.countTrojanCards = countTrojanCards;
+    }
+    public int getCountWormCards() {
+        return countWormCards;
+    }
+    public void setCountWormCards(int countWormCards) {
+        if((countWormCards<0)) this.countWormCards = 0;
+        else this.countWormCards = countWormCards;
+    }
+    public int getCountVirusCards() {
+        return countVirusCards;
+    }
+
+    public void setCountVirusCards(int countVirusCards) {
+        if((countVirusCards<0)) this.countVirusCards = 0;
+        else this.countVirusCards = countVirusCards;
+    }
+
+    public void handleDamageCount(CardType cardType) {
+        switch (cardType) {
+            case Spam -> countSpamCards--;
+            case Trojan -> countTrojanCards--;
+            case Worm -> countWormCards--;
+            case Virus -> countVirusCards--;
+        }
+    }
+
+    public void handleDamageCount(ArrayList<CardType> cardList) {
+        if(cardList.size() == 0) setCountSpamCards(0); //TODO correct?
+        else {
+            for (CardType cardType : cardList) {
+                handleDamageCount(cardType);
+            }
+        }
+
+    }
     protected CardType generateCardType(String imageDropped) {
         String[] a = imageDropped.split("/");
         String imageName = a[a.length - 1];
-        String cardName = imageName.substring(0, imageName.length() - 9);
-        return CardType.valueOf(cardName);
+        CardType cardName = CardType.valueOf(imageName.substring(0, imageName.length() - 9));
+        return cardName;
     }
 
     public ImageView getProgrammingImageView() {
