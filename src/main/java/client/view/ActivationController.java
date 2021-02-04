9117b5f24995050669e840e6c27095b24a4572cd
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-02-04 04:53:49 +0100

    shortened Activation phase with two inner views and getter for controller

--- src/main/java/client/view/ActivationController.java
+++ src/main/java/client/view/ActivationController.java
@@ -1,20 +1,28 @@
 package client.view;
 
+import game.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import utilities.ImageHandler;
 import utilities.JSONProtocol.body.PickDamage;
 import utilities.JSONProtocol.body.PlayIt;
 import utilities.JSONProtocol.body.SelectDamage;
 import utilities.enums.CardType;
+import utilities.enums.GameState;
 import utilities.enums.MessageType;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -22,61 +30,69 @@ import java.util.ArrayList;
  */
 
 public class ActivationController extends Controller {
-    private final ArrayList<CardType> pickedDamageCards = new ArrayList<>();
-    private int pickDamage;
+    private static final Logger logger = LogManager.getLogger();
     private int registerNr = 1;
-
     @FXML
     private Label register;
-    @FXML
-    private Label infoLabel;
-    @FXML
-    private ImageView currentCardImageView;
-    @FXML
-    private HBox selectedDamageHBox;
-    @FXML
-    private Button playItButton;
-    @FXML
-    private AnchorPane selectDamageAnchorPane;
-    @FXML
-    private AnchorPane playCardAnchorPane;
-    @FXML
-    private Button spamCardButton;
-    @FXML
-    private Button trojanCardButton;
-    @FXML
-    private Button virusCardButton;
-    @FXML
-    private Button wormCardButton;
-    @FXML
-    private Label damageInfoLabel;
-    public Label displayAction;
 
-    /**
-     * This method resets the class and gets called at the start of activationPhase
-     */
-    public void reset() {
-        registerNr = 1;
-        playItButton.setDisable(true);
-    }
+
+    public BorderPane innerActivationPhase;
+    public Pane pickDamagePane;
+    public Pane playCardPane;
+    public PickDamageController pickDamageController;
+    public PlayCardController playCardController;
 
     /**
      * This method initializes the ActivationController, sets important things visible
      */
     public void initialize() {
-        selectDamageAnchorPane.setVisible(false);
-        playItButton.setDisable(true);
+        constructPhaseViews();
+        reset();
     }
 
+    /**
+     * This method resets the class and gets called at the start of activationPhase
+     */
+    public void reset() {
+        changePhaseView("PlayIt");
+        registerNr = 1;
+        playCardController.getPlayItButton().setDisable(true);
+    }
 
-    public void setDisplayAction(ArrayList<MessageType> currentAction){
-        String text = " ";
-        for (MessageType message : currentAction){
-            text += message;
-            text += " ";
+    private void constructPhaseViews() {
+
+        FXMLLoader pickDamageLoader = new FXMLLoader(getClass().getResource("/view/innerViews/pickDamage.fxml"));
+        FXMLLoader playCardLoader = new FXMLLoader(getClass().getResource("/view/innerViews/playCard.fxml"));
+
+        try {
+            pickDamagePane = pickDamageLoader.load();
+            playCardPane = playCardLoader.load();
+
+            pickDamageController = pickDamageLoader.getController();
+            playCardController = playCardLoader.getController();
+
+            pickDamageController.setActivationController(this);
+        } catch (IOException e) {
+            logger.error("Inner phase View could not be loaded: " + e.getMessage());
         }
-        displayAction.setText(text);
     }
+
+    public void changePhaseView(String innerView) {
+        switch (innerView) {
+            case "Damage" -> { innerActivationPhase.setCenter(pickDamagePane);}
+            case "PlayIt" -> { innerActivationPhase.setCenter(playCardPane);}
+        }}
+
+
+    public PickDamageController getPickDamageController() {
+        return pickDamageController;
+    }
+
+    public PlayCardController getPlayCardController() {
+        return playCardController;
+    }
+
+
     /**
      * This method gets called by getting protocol currentCards and displays current card of player
      * it also increases register number for view
@@ -84,123 +100,12 @@ public class ActivationController extends Controller {
      * @param cardType current card of the player
      */
     public void currentCards(CardType cardType) {
-        currentCardImageView.setImage(new Image(getClass().getResource("/cards/programming/" + cardType + "-card.png").toString()));
+        playCardController.getCurrentCardImageView().setImage(new Image(getClass().getResource("/cards/programming/" + cardType + "-card.png").toString()));
         register.setText("Register " + registerNr);
         registerNr++;
     }
 
-    /**
-     * This method displays if player is current player and sets play It button disable (or not)
-     *
-     * @param turn
-     */
-    public void currentPlayer(boolean turn) {
-        if (turn) {
-            setInfoLabel("It's your turn! Click on the button to validate your card!");
-            playItButton.setDisable(false);
-        } else {
-            setInfoLabel("It's not your turn");
-            playItButton.setDisable(true);
-        }
-    }
-
-    @FXML
-    private void playItButton() {
-        client.sendMessage(new PlayIt());
-    }
-
-    private void setInfoLabel(String text) {
-        infoLabel.setText(text);
-    }
-
-    /**
-     * by getting protocol PickDamage the damageAnchorPane gets visible and it updates the
-     * count numbers on the different damageButtons
-     *
-     * @param pickDamage
-     */
-    public void pickDamage(PickDamage pickDamage) {
-        countSpam = getCountSpamCards();
-        countTrojan = getCountTrojanCards();
-        countVirus = getCountVirusCards();
-        countWorm = getCountWormCards();
-        updateDamageCountLabel(CardType.Spam);
-        updateDamageCountLabel(CardType.Worm);
-        updateDamageCountLabel(CardType.Trojan);
-        updateDamageCountLabel(CardType.Virus);
-
-        selectedDamageHBox.getChildren().clear();
-        playCardAnchorPane.setVisible(false);
-        selectDamageAnchorPane.setVisible(true);
-        damageInfoLabel.setText("You have to pick " + pickDamage.getCount() + " damage cards");
-        this.pickDamage = pickDamage.getCount();
-    }
-
-    private int countSpam;
-    private int countTrojan;
-    private int countVirus;
-    private int countWorm;
 
 
-    private void updateDamageCountLabel(CardType cardType){
-        switch (cardType){
-            case Spam->{
-                spamCardButton.setText(String.valueOf(countSpam));
-                if ((countSpam == 0)) spamCardButton.setDisable(true);
-                else spamCardButton.setDisable(false);
-            }
-            case Trojan -> {
-                trojanCardButton.setText(String.valueOf(countTrojan));
-                if (countTrojan == 0) trojanCardButton.setDisable(true);
-                else trojanCardButton.setDisable(false);
-            }
-            case Virus -> {
-                virusCardButton.setText(String.valueOf(countVirus));
-                if (countVirus == 0) virusCardButton.setDisable(true);
-                else  virusCardButton.setDisable(false);
-            }
-            case Worm -> {
-                wormCardButton.setText(String.valueOf(countWorm));
-                if (countWorm == 0) wormCardButton.setDisable(true);
-                else wormCardButton.setDisable(false);
-            }
-        }
-    }
-    @FXML
-    private void damageButtonClicked(ActionEvent actionEvent) {
-        CardType clickedButton = null;
-        if (actionEvent.getSource().equals(spamCardButton)) {
-            clickedButton = CardType.Spam;
-            countSpam--;
-        }
-        else if (actionEvent.getSource().equals(trojanCardButton))  {
-            clickedButton = CardType.Trojan;
-            countTrojan--;
-        }
-        else if (actionEvent.getSource().equals(virusCardButton)) {
-            clickedButton = CardType.Virus;
-            countVirus--;
-        }
-        else if (actionEvent.getSource().equals(wormCardButton)) {
-            clickedButton = CardType.Worm;
-            countWorm--;
-        }
-        checkDamageReady(clickedButton);
-    }
-
-    private void checkDamageReady(CardType card) {
-        updateDamageCountLabel(card);
-        pickedDamageCards.add(card);
-        String path = "/cards/programming/" + card + "-card.png";
-        ImageView selectedDamageCard = ImageHandler.createImageView(path, 70, 100);
-        selectedDamageHBox.getChildren().add(selectedDamageCard);
-        if (pickedDamageCards.size() == pickDamage) {
-            client.sendMessage(new SelectDamage(pickedDamageCards));
-            pickDamage = 0;
-            pickedDamageCards.clear();
-            playCardAnchorPane.setVisible(true);
-            selectDamageAnchorPane.setVisible(false);
-        }
-    }
 }
 
