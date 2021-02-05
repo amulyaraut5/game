d5532800b76d8279d3d0eb5cf2e60fdefd3976de
Sarah Berbuir <berbuir@cip.ifi.lmu.de>
2021-02-05 12:40:52 +0100

    removed calculating of prior actions

--- src/main/java/client/view/PlayCardController.java
+++ src/main/java/client/view/PlayCardController.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 
 public class PlayCardController extends Controller {
 
-    public Label moveInfo;
     public Label displayAction;
     @FXML
     private ImageView currentCardImageView;
@@ -31,24 +30,6 @@ public class PlayCardController extends Controller {
         return currentCardImageView;
     }
 
-    public void setDisplayAction(ArrayList<MessageType> currentAction) {
-        StringBuilder text = new StringBuilder(" ");
-        for (MessageType message : currentAction) {
-            text.append(message);
-            text.append(" ");
-        }
-        String displayAction = "";
-        switch (text.toString()) {
-            case " Movement Movement Movement " -> displayAction = "You moved 3";
-            case " Movement Movement " -> displayAction = "You moved 2";
-            case " Movement " -> displayAction = "You moved 1";
-            case " PlayerTurning " -> displayAction = "You turned";
-            case " PlayerTurning PlayerTurning " -> displayAction = "You performed an UTurn";
-            case " Energy " -> displayAction = "You got energy";
-            default -> displayAction = text.toString();
-        }
-        moveInfo.setText(displayAction);
-    }
 
     /**
      * This method displays if player is current player and sets play It button disable (or not)
